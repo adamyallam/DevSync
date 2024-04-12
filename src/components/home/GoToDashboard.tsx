3b58202fb67a60d12828e40a38556ee65f9495a1
Adam Allam <adamyallam@gmail.com>
2024-04-12 16:59:06 -0500

    Optimized part of homepage for small screens

--- src/components/home/GoToDashboard.tsx
+++ src/components/home/GoToDashboard.tsx
@@ -7,15 +7,19 @@ export default function GoToDashboard(){
 
   return (
     <div className=''>
-        <div className="grid grid-cols-3 grid-rows-2 items-center border-b-2 border-black">
+        <div className="grid grid-cols-4 grid-rows-2 items-center border-b-2 border-black">
             <div className="flex justify-start col-span-2">
-                <h1 className='text-4xl md:text-6xl ml-5'>Dashboard</h1>
+                <h1 className='mt-10 text-3xl ml-5 sm:text-4xl md:text-5xl lg:ml-28'>Dashboard</h1>
             </div>
-            <div className='flex justify-center row-span-2 pl-5 pb-5 pt-5 mt-10 border-black text-center'>
-                <h2>Your workspace all in one place. Create and manage projects, check schedules, message other users and much more!</h2>
+            <div className='flex justify-center text-center pl-5 pb-5 pt-5 col-span-2 row-span-2'>
+                <h2 className='md:hidden'>Your workspace all in one place. Create and manage projects, check schedules, message other users and much more!</h2>
+                <h2 className='hidden md:block'>Your workspace all in one place. Create and manage projects, check schedules, message other users and much more!
+                    Its simple use allows anyone to use and is great for teams with varying skill levels. Helpp keep track of projects with
+                    Project Tracker!
+                </h2>
             </div>
-            <div className='flex justify-start col-span-2'>
-                <button className='text-xl ml-6 mb-10'>Go to dashboard ➞</button>
+            <div className='col-span-2'>
+                <button className='text-base mb-20 ml-5 lg:ml-28'>Go to dashboard ➞</button>
             </div>
         </div>
     </div>
--- src/components/home/Slogan.tsx
+++ src/components/home/Slogan.tsx
@@ -7,15 +7,15 @@ export default function Slogan(){
 
   return (
     <div className=''>
-      <div className='grid grid-cols-1 grid-rows-3 gap-2 m-4'>
+      <div className='grid grid-cols-1 grid-rows-3 gap-2 mt-4'>
         <div className='flex justify-center'>
           <h1 className='text-3xl'>SLOGAN</h1>
         </div>
-        <div className='flex justify-center'>
+        <div className='flex justify-center text-center'>
           <h1>Brief description of what app does and the usage</h1>
         </div>
         <div className='flex justify-center'>
-        <button className='border-2 border-black p-1 m-1'>Get Started</button>
+        <button className='border-2 border-black p-1 '>Get Started</button>
         </div>
       </div>
     </div>
