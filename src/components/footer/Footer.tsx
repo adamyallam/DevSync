b78c6ad901605c342816b682b98f536648c7dfc5
Adam Allam <adamyallam@gmail.com>
2024-04-20 13:42:05 -0500

    fully optimized footer for all screen sizes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,11 +7,19 @@ export default function GoToDashboard(){
 
   return (
     <div>
-      <div className='grid grid-cols-1 grid-rows-3 bg-gray-800 h-64'>
-        <div className='flex justify-center'>
-          <Image className='w-16 h-16 mt-7' src={Logo} alt="" />
+      <div className='grid grid-cols-1 grid-rows-3 bg-gray-800 h-64 md:grid-cols-3 md:grid-rows-4 xl:grid-rows-5'>
+        <div className='flex justify-center md:mt-7 xl:ml-20'>
+          <Image className='w-16 h-16 mt-7 md:hidden' src={Logo} alt="" />
+          <Image className='hidden md:block w-36 h-20 mt-4 border-b-2 border-b-gray-500 xl:w-40 xl:h-24 2xl:ml-20' src={LogoBig} alt="" />
         </div>
-        <div className="flex justify-center text-center text-gray-300 mt-5">
+        <Socials divClass='hidden xl:flex col-start-1 row-start-4 justify-center ml-20 2xl:ml-40' imgClass='h-10 w-10' />
+        <div className='hidden md:flex justify-center col-start-1 row-start-2 mt-16 ml-16 w-1/2 lg:justify-end xl:row-start-3 '>
+          <Socials divClass='flex mt-2 lg:mr-1 xl:hidden' imgClass='h-10 w-10' />
+        </div>
+        <div className='hidden md:flex items-end text-gray-300 justify-center mt-4'>
+         <p>Navigate</p>
+        </div>
+        <div className="flex justify-center text-center text-gray-300 mt-5 md:col-start-2 md:row-start-2">
           <ul>
             <li>
               <a href='/Dashboard'>Dashboard</a>
@@ -30,9 +38,17 @@ export default function GoToDashboard(){
             </li>
           </ul>
          </div>
-
+          <div className='hidden md:block mt-14'>
+            <p className='text-gray-300 mb-2'>Join our newsletter!</p>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="border-2 border-black p-1 w-56 h-10 mb-1"
+            />
+            <button className="bg-black text-white p-1 w-20 h-10">Sign Up</button>
+          </div>
       </div>
-      <div className='grid grid-cols-1 grid-rows-4 bg-gray-700'>
+      <div className='grid grid-cols-1 grid-rows-4 bg-gray-700 md:hidden'>
         <p className='flex justify-center text-gray-200 mt-6 mb-3'>Stay in touch by joining our newsletter!</p>
         <div className="flex justify-center">
            <input
