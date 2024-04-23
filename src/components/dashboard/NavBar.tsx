e6bb1bd690464da49dfaf0d931e9d9f0e4eca078
Adam Allam <adamyallam@gmail.com>
2024-04-23 16:58:23 -0500

    Added ability to select pages

--- src/components/dashboard/NavBar.tsx
+++ src/components/dashboard/NavBar.tsx
@@ -1,32 +1,22 @@
 'use client'
-import { useRouter } from 'next/navigation'
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {MenuIcon, X, Instagram, Twitter, Linkedin, Home, CircleCheck, Inbox, CalendarCheck, UserRound} from 'lucide-react'
 
 
 
 
-export default function DashTopBar() {
-  const [isOpen, setIsOpen] = useState(false);
-  // const [hasScrolled, setHasScrolled] = useState(false);
+export default function NavBar() {
+  const [isOpen, setIsOpen] = useState(true);
+  const [page, setPage] = useState('Home');
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
-    // document.body.style.overflow = isOpen ? 'unset' : 'hidden';
   };
 
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     setHasScrolled(window.scrollY > 0);
-  //   };
-
-  //   window.addEventListener('scroll', handleScroll);
-
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll);
-  //   };
-  // }, []);
-
+  const selectedPage = (pageTitle: string) => {
+    setPage(pageTitle)
+    let test = `ml-6 border-2 w-40`
+  }
 
   return (
     <div className='fixed w-full top-0 z-10'>
@@ -45,54 +35,59 @@ export default function DashTopBar() {
       </div>
 
       {isOpen && (
-        <div className='border-t-2 border-gray-600'>
-          <div className='grid grid-rows-8 h-screen bg-gray-700 w-60'>
+        <div className=' h-screen bg-gray-700 w-60 border-t-2 border-gray-600'>
+          <div className='grid grid-rows-5'>
+            <div className='flex flex-col mt-7 mb-7 text-gray-200'>
+              <button className={`flex items-center h-8 ${page === 'Home' ? 'bg-black bg-opacity-60 rounded-lg w-52 h-8 ml-4 pl-4' : 'ml-8 hover:bg-gray-800 hover:bg-opacity-60 hover:rounded-lg hover:w-52 hover:h-8 hover:ml-4 hover:pl-4'}`} 
+                onClick={() => selectedPage('Home')}>
+                <Home size={20} color="#e5e7eb" strokeWidth={1.5}/>
+                <span className='ml-1 text-sm'>Home</span>
+              </button>
 
-            <div className='flex flex-col mt-8 text-gray-200'>
-              <div className='flex items-center ml-8 mb-1'>
-                <Home size={18} color="#e5e7eb" strokeWidth={1.5}/>
-                <button className='ml-2'>Home</button>
-              </div>
+              <button className={`flex items-center h-8 ${page === 'Tasks' ? 'bg-black bg-opacity-60 rounded-lg w-52 h-8 ml-4 pl-4' : 'ml-8 hover:bg-gray-800 hover:bg-opacity-60 hover:rounded-lg hover:w-52 hover:h-8 hover:ml-4 hover:pl-4'}`} 
+                onClick={() => selectedPage('Tasks')}>
+                <CircleCheck size={20} color="#e5e7eb" strokeWidth={1.5}/>
+                <span className='ml-1 text-sm'>Tasks</span>
+              </button>
 
-              <div className='flex items-center ml-8 mb-1'>
-                <CircleCheck size={18} color="#e5e7eb" strokeWidth={1.5}/>
-                <button className='ml-2'>My Tasks</button>
-              </div>
-
-              <div className='flex items-center ml-8 mb-1'>
-                <Inbox size={18} color="#e5e7eb" strokeWidth={1.5}/>
-                <button className='ml-2'>Inbox</button>
-              </div>
+              <button className={`flex items-center h-8 ${page === 'Inbox' ? 'bg-black bg-opacity-60 rounded-lg w-52 h-8 ml-4 pl-4' : 'ml-8 hover:bg-gray-800 hover:bg-opacity-60 hover:rounded-lg hover:w-52 hover:h-8 hover:ml-4 hover:pl-4'}`} 
+                onClick={() => selectedPage('Inbox')}>
+                <Inbox size={20} color="#e5e7eb" strokeWidth={1.5}/>
+                <span className='ml-1 text-sm'>Inbox</span>
+              </button>
             </div>
 
-            <div className='mt-10 text-gray-200'>
-              <h1 className='ml-8 font-bold'>Insights</h1>
+            <div className=' text-gray-200'>
+              <h1 className='ml-8 mb-1 font-bold'>Insights</h1>
 
-              <div className='flex items-center ml-10 mt-1'>
-                <CalendarCheck size={18} color="#e5e7eb" strokeWidth={1.5}/>
-                <button className='ml-2'>Calendar</button>
-              </div>
+              <button className={`flex items-center  h-8 ${page === 'Calendar' ? 'bg-black bg-opacity-60 rounded-lg w-52 h-8 ml-5 pl-5' : 'ml-10 hover:bg-gray-800 hover:bg-opacity-60 hover:rounded-lg hover:w-52 hover:h-8 hover:ml-5 hover:pl-5'}`} 
+                onClick={() => selectedPage('Calendar')}>
+                <CalendarCheck size={20} color="#e5e7eb" strokeWidth={1.5}/>
+                <span className='ml-1 text-sm'>Calendar</span>
+              </button>
             </div>
 
-            <div className='mt-8 text-gray-200'>
-                <h1 className='ml-8 font-bold'>Projects</h1>
+            <div className=' text-gray-200'>
+              <h1 className='ml-8 mb-1 font-bold'>Projects</h1>
 
-                <div className='flex items-center ml-10 mt-1'>
-                  <div className='border-2 bg-white rounded-md w-4 h-4'></div>
-                  <button className='ml-2'>Project Name</button>
-              </div>
+              <button className={`flex items-center h-8 ${page === 'ProjectName' ? 'bg-black bg-opacity-60 rounded-lg w-52 h-8 ml-5 pl-5' : 'ml-10 hover:bg-gray-800 hover:bg-opacity-60 hover:rounded-lg hover:w-52 hover:h-8 hover:ml-5 hover:pl-5'}`} 
+                onClick={() => selectedPage('ProjectName')}>
+                <div className='border-2 bg-white rounded-md w-4 h-4' />
+                <span className='ml-2 text-sm'>ProjectName</span>
+              </button>
             </div>
 
-            <div className='mt-8 text-gray-200'>
-              <h1 className='ml-8 font-bold'>Team</h1>
+            <div className='text-gray-200'>
+              <h1 className='ml-8 mb-1 font-bold'>Team</h1>
 
-              <div className='flex items-center ml-10 mt-1'>
-                <UserRound size={18} color="#e5e7eb" strokeWidth={1.5}/>
-                <button className='ml-2'>My Workspace</button>
-              </div>
+              <button className={`flex items-center h-8 ${page === 'Workspace' ? 'bg-black bg-opacity-60 rounded-lg w-52 h-8 ml-5 pl-5' : 'ml-10 hover:bg-gray-800 hover:bg-opacity-60 hover:rounded-lg hover:w-52 hover:h-8 hover:ml-5 hover:pl-5'}`} 
+                onClick={() => selectedPage('Workspace')}>
+                <UserRound size={20} color="#e5e7eb" strokeWidth={1.5}/>
+                <span className='ml-1 text-sm'>Workspace</span>
+              </button>
             </div>
 
-            <div className='border-t-2 row-start-7'>
+            <div className='border-t-2 mt-9'>
               <div className='flex justify-center mt-4'>
                 <button className='border-2 w-48 h-10'>Create Project</button>
               </div>
@@ -109,5 +104,3 @@ export default function DashTopBar() {
     </div>
   )
 }
-
-
