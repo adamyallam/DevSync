f162f05c1651906bc30a85d8e4df9a801ee8d9d7
Adam Allam <adamyallam@gmail.com>
2024-04-24 15:47:53 -0500

    Created "Home" component for dashboard

---
+++ src/components/dashboard/Home.tsx
@@ -0,0 +1,23 @@
+
+
+
+export const Home = () => { 
+
+    const page = true;
+    
+    if (page) {
+    return (
+        <div className="bg-orange-800">
+            <p>Random Text</p>
+        </div>
+    )
+    } else {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Home
