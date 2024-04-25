0d04c8c86a0866d950442a19de3a910197c6d757
Adam Allam <adamyallam@gmail.com>
2024-04-24 22:46:27 -0500

    made class edits

--- src/components/dashboard/pages/Home.tsx
+++ src/components/dashboard/pages/Home.tsx
@@ -4,7 +4,7 @@
 export const Home = () => { 
 
     return (
-        <div className="mt-48 mx-auto w-72 border-l-2 border-l-black">
+        <div className="mt-48 border-l-2 border-l-black">
             <h1 className="text-2xl">Home Page</h1>
         </div>
     )
