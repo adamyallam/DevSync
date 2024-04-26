82638b8dfa65553a8eab2b8f97c0d55bcb21bbba
Adam Allam <adamyallam@gmail.com>
2024-04-26 17:12:57 -0500

    added pages for the dashboard with starter code

---
+++ src/app/(routes)/dashboard/workspace/page.tsx
@@ -0,0 +1,26 @@
+import { authOptions } from 'src/app/api/auth/[...nextauth]/route'
+import {getServerSession} from 'next-auth'
+import Dashboard from 'src/components/dashboard/Dashboard'
+
+
+export default async function App(){
+  const session = await getServerSession(authOptions)
+
+  if (/*Remove "!" when done testing*/ !session) {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h1 className="text-red-700">Please signin before accessing your Dashboard</h1>
+      <a href='http://localhost:3000/'> 
+          <button type="button">Home</button> 
+      </a>
+    </div>
+  )
+
+}
