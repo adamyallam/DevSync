30d688a906896ef8f120379921707a20ceae5a12
Adam Allam <adamyallam@gmail.com>
2024-04-28 12:05:37 -0500

    Changed classes to make text visible

--- src/app/(routes)/pages/about/page.tsx
+++ src/app/(routes)/pages/about/page.tsx
@@ -3,7 +3,7 @@
 const about = () => {
     return (
         <div>
-            <h1 className="text-3xl mb-4 p-3 mt-4 border-b-2 border-gray-400 w-2/5 mx-auto">About Us</h1>
+            <h1 className="flex justify-center text-3xl mb-96 mt-24">About Us</h1>
         </div>
     )
 }
