4193ce6fa7f4670ab269312bd2a6ba9981da3d4c
Adam Allam <adamyallam@gmail.com>
2024-07-19 16:54:53 -0500

    Adjusted formatting and implemented a way to display the date and period of day

--- src/components/dashboard/pages/Home.tsx
+++ src/components/dashboard/pages/Home.tsx
@@ -1,5 +1,7 @@
 'use client'
 import { useContext } from 'react';;
+import { getCurrentDate } from '@/utils/todaysDate';
+import { getDayPeriod } from '@/utils/todaysDate';
 
 // Component Imports
 import { OpenContext } from '@/components/context/OpenContext';
@@ -13,9 +15,14 @@ export const Home: React.FC<Props> = (props) => {
     
     return (
         <div className={`transition-all duration-300 ${isOpen ? 'ml-64' : 'ml-8'}`}>
-            <h1 className='mt-20 text-2xl'>Welcome, {props.name}</h1>
+            <h1 className='mt-20 text-2xl'>Home</h1>
+            <div>
+                <p>{getCurrentDate()}</p>
+                <h1>Good {getDayPeriod()}, {props.name}</h1>
+            </div>
         </div>
     )
 }
+
 
 export default Home
