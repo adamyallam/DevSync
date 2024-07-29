6551978de1d1214876f15400edd4e6f5e985ff78
Adam Allam <adamyallam@gmail.com>
2024-07-29 16:30:56 -0500

    Finished layout for card title

--- src/components/dashboard/pages/home/TasksCard.tsx
+++ src/components/dashboard/pages/home/TasksCard.tsx
@@ -6,16 +6,20 @@ export const TasksCard = () => {
     
     return (
       <div className="w-5/12 h-96 border-2 border-gray-200 rounded-lg shadow-sm">
-        <div className="grid grid-cols-3 grid-rows-2">
-          <div className="w-16 h-16 ml-6 mt-5 border-2 border-black bg-gray-200 rounded-full">
-            <p className="flex justify-center mt-4">PFP</p>
+        <div className="flex items-center p-8 ml-5">
+
+          <div className="w-14 h-14 mb-5 border-2 border-black bg-gray-200 rounded-full"></div> {/* Profile Photo Placeholder */}
+
+          <div className="flex flex-col gap-3 ml-5">
+            <h1 className="text-xl">My Tasks</h1>
+
+            <div className="flex gap-4">
+              <p className="text-sm">Upcoming</p>
+              <p className="text-sm">Overdue</p>
+              <p className="text-sm">Completed</p>
+            </div>
           </div>
-          <h1 className="mt-9 text-lg">My Tasks</h1>
-          <div className="flex justify-start gap-2 row-start-2">
-            <p className="text-sm">Upcoming</p>
-            <p className="text-sm">Overdue</p>
-            <p className="text-sm">Completed</p>
-          </div>
+
         </div>
       </div>
     )
