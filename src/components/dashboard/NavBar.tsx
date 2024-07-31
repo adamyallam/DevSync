c13161bda3429890b0bfa448fa7360c837e8d5b2
Adam Allam <adamyallam@gmail.com>
2024-07-31 16:38:24 -0500

    Navbar dynamically adds the title of page to the screen

--- src/components/dashboard/NavBar.tsx
+++ src/components/dashboard/NavBar.tsx
@@ -8,6 +8,7 @@ import { Transition } from '@headlessui/react'
 
 // Component Imports
 import { OpenContext } from '@/components/context/OpenContext';
+import { pageTitle } from '@/utils/pageTitle';
 
 interface Props {
   toggleButton: React.ReactNode
@@ -15,10 +16,11 @@ interface Props {
 
 export const NavBar: React.FC<Props> = ({toggleButton}) => {
   const isOpen = useContext(OpenContext);
-  const pathname = usePathname()
-
 
   return (
+    <div>
+      <h1 className={`transition-all duration-300 text-2xl ${isOpen ? 'mt-20 ml-8 translate-x-60' : 'mt-20 ml-8'}`}>{pageTitle()}</h1>
+    
     <div className='fixed w-full top-0 z-10'>
       <div className='grid grid-cols-3 bg-gray-700 p-3'>
         
@@ -44,19 +46,19 @@ export const NavBar: React.FC<Props> = ({toggleButton}) => {
         <div className='h-screen bg-gray-700 w-60 border-t-2 border-gray-600'>
           <div className='flex flex-col mt-7 text-gray-200'>
             <div className='mb-7'>
-                <Link href='/dashboard/home' className={`flex items-center h-8 ${pathname === '/dashboard/home' ? 'bg-black bg-opacity-60 rounded-lg w-52 h-8 ml-4 pl-4' : 'ml-8 hover:bg-gray-800 hover:bg-opacity-60 hover:rounded-lg hover:w-52 hover:h-8 hover:ml-4 hover:pl-4'}`}>
+                <Link href='/dashboard/home' className={`flex items-center h-8 ${pageTitle() === 'Home' ? 'bg-black bg-opacity-60 rounded-lg w-52 h-8 ml-4 pl-4' : 'ml-8 hover:bg-gray-800 hover:bg-opacity-60 hover:rounded-lg hover:w-52 hover:h-8 hover:ml-4 hover:pl-4'}`}>
 
                   <Home size={20} color="#e5e7eb" strokeWidth={1.5}/>
                   <span className='ml-1 text-sm'>Home</span>
 
                 </Link>
 
-              <Link href='/dashboard/tasks' className={`flex items-center h-8 ${pathname === '/dashboard/tasks' ? 'bg-black bg-opacity-60 rounded-lg w-52 h-8 ml-4 pl-4' : 'ml-8 hover:bg-gray-800 hover:bg-opacity-60 hover:rounded-lg hover:w-52 hover:h-8 hover:ml-4 hover:pl-4'}`}>
+              <Link href='/dashboard/tasks' className={`flex items-center h-8 ${pageTitle() === 'Tasks' ? 'bg-black bg-opacity-60 rounded-lg w-52 h-8 ml-4 pl-4' : 'ml-8 hover:bg-gray-800 hover:bg-opacity-60 hover:rounded-lg hover:w-52 hover:h-8 hover:ml-4 hover:pl-4'}`}>
                   <CircleCheck size={20} color="#e5e7eb" strokeWidth={1.5}/>
                   <span className='ml-1 text-sm'>Tasks</span>
               </Link>
 
-              <Link href='/dashboard/inbox' className={`flex items-center h-8 ${pathname === '/dashboard/inbox' ? 'bg-black bg-opacity-60 rounded-lg w-52 h-8 ml-4 pl-4' : 'ml-8 hover:bg-gray-800 hover:bg-opacity-60 hover:rounded-lg hover:w-52 hover:h-8 hover:ml-4 hover:pl-4'}`} >
+              <Link href='/dashboard/inbox' className={`flex items-center h-8 ${pageTitle() === 'Inbox' ? 'bg-black bg-opacity-60 rounded-lg w-52 h-8 ml-4 pl-4' : 'ml-8 hover:bg-gray-800 hover:bg-opacity-60 hover:rounded-lg hover:w-52 hover:h-8 hover:ml-4 hover:pl-4'}`} >
                 
                   <Inbox size={20} color="#e5e7eb" strokeWidth={1.5}/>
                   <span className='ml-1 text-sm'>Inbox</span>
@@ -67,7 +69,7 @@ export const NavBar: React.FC<Props> = ({toggleButton}) => {
             <div className='mb-7 text-gray-200'>
               <h1 className='ml-8 mb-1 font-bold'>Insights</h1>
 
-              <Link href='/dashboard/calendar' className={`flex items-center  h-8 ${pathname === '/dashboard/calendar' ? 'bg-black bg-opacity-60 rounded-lg w-52 h-8 ml-5 pl-5' : 'ml-10 hover:bg-gray-800 hover:bg-opacity-60 hover:rounded-lg hover:w-52 hover:h-8 hover:ml-5 hover:pl-5'}`}>
+              <Link href='/dashboard/calendar' className={`flex items-center  h-8 ${pageTitle() === 'Calendar' ? 'bg-black bg-opacity-60 rounded-lg w-52 h-8 ml-5 pl-5' : 'ml-10 hover:bg-gray-800 hover:bg-opacity-60 hover:rounded-lg hover:w-52 hover:h-8 hover:ml-5 hover:pl-5'}`}>
                   <CalendarCheck size={20} color="#e5e7eb" strokeWidth={1.5}/>
                   <span className='ml-1 text-sm'>Calendar</span>
               </Link>
@@ -76,7 +78,7 @@ export const NavBar: React.FC<Props> = ({toggleButton}) => {
             <div className='mb-7 text-gray-200'>
               <h1 className='ml-8 mb-1 font-bold'>Projects</h1>
 
-              <Link href='/dashboard/project' className={`flex items-center  h-8 ${pathname === '/dashboard/project' ? 'bg-black bg-opacity-60 rounded-lg w-52 h-8 ml-5 pl-5' : 'ml-10 hover:bg-gray-800 hover:bg-opacity-60 hover:rounded-lg hover:w-52 hover:h-8 hover:ml-5 hover:pl-5'}`}>
+              <Link href='/dashboard/project' className={`flex items-center  h-8 ${pageTitle() === 'Project' ? 'bg-black bg-opacity-60 rounded-lg w-52 h-8 ml-5 pl-5' : 'ml-10 hover:bg-gray-800 hover:bg-opacity-60 hover:rounded-lg hover:w-52 hover:h-8 hover:ml-5 hover:pl-5'}`}>
                   <div className='border-2 bg-white rounded-md w-4 h-4' />
                   <span className='ml-2 text-sm'>ProjectName</span>
               </Link>
@@ -85,7 +87,7 @@ export const NavBar: React.FC<Props> = ({toggleButton}) => {
             <div className='text-gray-200 '>
               <h1 className='ml-8 mb-1 font-bold'>Team</h1>
 
-              <Link href='/dashboard/workspace' className={`flex items-center  h-8 ${pathname === '/dashboard/workspace' ? 'bg-black bg-opacity-60 rounded-lg w-52 h-8 ml-5 pl-5' : 'ml-10 hover:bg-gray-800 hover:bg-opacity-60 hover:rounded-lg hover:w-52 hover:h-8 hover:ml-5 hover:pl-5'}`}>
+              <Link href='/dashboard/workspace' className={`flex items-center  h-8 ${pageTitle() === 'Workspace' ? 'bg-black bg-opacity-60 rounded-lg w-52 h-8 ml-5 pl-5' : 'ml-10 hover:bg-gray-800 hover:bg-opacity-60 hover:rounded-lg hover:w-52 hover:h-8 hover:ml-5 hover:pl-5'}`}>
 
                   <UserRound size={20} color="#e5e7eb" strokeWidth={1.5}/>
                   <span className='ml-1 text-sm'>Workspace</span>
@@ -107,6 +109,7 @@ export const NavBar: React.FC<Props> = ({toggleButton}) => {
         </div>
       </Transition>
 
+    </div>
     </div>
   )
 }
