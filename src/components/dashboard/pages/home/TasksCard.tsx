fe3a21559a061fc6fb9f1de4ae4aab854dcb14bd
Adam Allam <adamyallam@gmail.com>
2024-08-01 16:51:26 -0500

    finished Tasks card's layout

--- src/components/dashboard/pages/home/TasksCard.tsx
+++ src/components/dashboard/pages/home/TasksCard.tsx
@@ -6,20 +6,41 @@ export const TasksCard = () => {
     
     return (
       <div className="w-4/12 h-96 border-2 border-gray-200 rounded-lg shadow-sm">
-        <div className="flex items-center p-8 ml-5">
 
-          <div className="w-14 h-14 mb-5 border-2 border-black bg-gray-200 rounded-full"></div> {/* Profile Photo Placeholder */}
+        <div className="flex items-center mt-8 ml-12">
+          <div className="w-14 h-14 mb-5 border-2 border-gray-600 bg-gray-200 rounded-full"></div> {/* Profile Photo Placeholder */}
 
           <div className="flex flex-col gap-3 ml-5">
             <h1 className="text-xl">My Tasks</h1>
 
             <div className="flex gap-4">
-              <p className="text-sm">Upcoming</p>
-              <p className="text-sm">Overdue</p>
-              <p className="text-sm">Completed</p>
-            </div>
-          </div>
+              <div className="flex-col">
+                <button className="text-sm">Upcoming</button>
+                <div className="border-2 mt-3 border-gray-600"></div>
+              </div>
 
+              <div className="flex-col">
+                <button className="text-sm">Overdue</button>
+                <div className="border-2 mt-3 border-gray-600"></div>
+              </div>
+
+              <div className="flex-col">
+                <button className="text-sm">Completed</button>
+                <div className="border-2 mt-3 border-gray-600"></div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+        <div className="border-t-2 border-gray-300"></div> {/* Separator */}
+        <p className="text-sm mt-2 ml-3 pb-3">Create task</p>
+        <div className="flex flex-col">
+          <button className="flex justify-start ml-5 p-1 w-11/12 border-t-2 border-b-2">Task</button>
+          <button className="flex justify-start ml-5 p-1 w-11/12 border-b-2">Task</button>
+          <button className="flex justify-start ml-5 p-1 w-11/12 border-b-2">Task</button>
+          <button className="flex justify-start ml-5 p-1 w-11/12 border-b-2">Task</button>
+          <button className="flex justify-start ml-5 p-1 w-11/12 border-b-2">Task</button>
         </div>
       </div>
     )
