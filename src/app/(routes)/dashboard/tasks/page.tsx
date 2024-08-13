e41a02ce77d8ddb4a228fc15890e2c6492486e8f
Adam Allam <adamyallam@gmail.com>
2024-08-13 16:26:02 -0500

    added pagetitle to tasks component

--- src/app/(routes)/dashboard/tasks/page.tsx
+++ src/app/(routes)/dashboard/tasks/page.tsx
@@ -2,11 +2,13 @@
 // Component Imports
 import Header from '@/components/dashboard/pages/tasks/Header'
 import ContentTransition from '@/components/dashboard/ContentTransition'
+import { PageTitle } from '@/components/dashboard/PageTitle'
 
 export default async function tasks() {
 
   return (
     <div>
+      <PageTitle />
       <ContentTransition>
         <Header />
       </ContentTransition>
