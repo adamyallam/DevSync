2854a444b00386f20adaa153659622de2c1dc136
Adam Allam <adamyallam@gmail.com>
2024-08-15 15:49:14 -0500

    refactored code and made filter sizes transition smoothly when sidebar opens and closes

--- src/components/dashboard/pages/tasks/TaskManagement.tsx
+++ src/components/dashboard/pages/tasks/TaskManagement.tsx
@@ -25,13 +25,13 @@ export const TaskManagement = () => {
       </div>
 
       <Transition transition="translate-x-60">
-        <div className={`grid grid-cols-12 grid-rows-1 border-b-2 border-t-2 border-gray-300 gap-2 ml-8 mt-4 h-10 ${isOpen ? 'mr-72' : 'mr-8'}`}>
-          <div className={`flex justify-between col-span-4 ml-2 border-r-2}`}>
+        <div className={`grid grid-cols-12 grid-rows-1 border-b-2 border-t-2 border-gray-300 gap-2 ml-8 mt-4 h-10 transition-all duration-300 ${isOpen ? 'mr-72' : 'mr-8'}`}>
+          <div className={`flex justify-between col-span-4 ml-2 border-r-2 border-gray-300}`}>
             <button className="text-xs">Task name</button>
             <button className="mr-2"><ChevronDown size={16} className="mt-1"/></button>
           </div>
 
-          <div className={`flex justify-between border-r-2 ${isOpen ? 'col-span-2' : 'col-span-2'}`}>
+          <div className={`flex justify-between border-r-2 col-span-2`}>
             <button className="text-xs">Due date</button>
             <button className="mr-2"><ChevronDown size={16} className="mt-1"/></button>
           </div>
@@ -41,12 +41,12 @@ export const TaskManagement = () => {
             <button className="mr-2"><ChevronDown size={16} className="mt-1"/></button>
           </div>
 
-          <div className={`flex justify-between items-center border-r-2 ${isOpen ? 'col-span-2' : 'col-span-2'}`}>
+          <div className={`flex justify-between items-center border-r-2 col-span-2`}>
             <p className="text-xs">Task visibility</p>
             <button className="mr-2"><ChevronDown size={16} className="mt-1"/></button>
           </div>
 
-          <div className={`flex justify-between items-center ${isOpen ? 'col-span-2' : 'col-span-2'}`}>
+          <div className={`flex justify-between items-center col-span-2`}>
             <p className="text-xs">Collaborators</p>
             <button className="mr-2"><ChevronDown size={16} className="mt-1"/></button>
           </div>
