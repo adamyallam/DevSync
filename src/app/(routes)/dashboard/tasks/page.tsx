89f85a5690ed03ee8d7c88f2006dfcdfbaec2448
Adam Allam <adamyallam@gmail.com>
2024-08-15 16:48:15 -0500

    Added new components and a simple h1 to make temporary space

--- src/app/(routes)/dashboard/tasks/page.tsx
+++ src/app/(routes)/dashboard/tasks/page.tsx
@@ -3,6 +3,8 @@
 import Header from '@/components/dashboard/pages/tasks/Header'
 import Transition from '@/components/dashboard/Transition'
 import TaskManagement from '@/components/dashboard/pages/tasks/TaskManagement'
+import MyTasks from '@/components/dashboard/pages/tasks/MyTasks'
+import Task from '@/components/dashboard/pages/tasks/Task'
 
 export default async function tasks() {
 
@@ -14,6 +16,11 @@ export default async function tasks() {
           </div>
       </Transition>
       <TaskManagement />
+      <Transition transition='translate-x-60'>
+        <MyTasks />
+      </Transition>
+      <Task />
+      <h1 className='mt-20'>Making space</h1>
     </div>
   )
 
