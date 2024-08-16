e7492e90de96b177faec1015ffca030b4b35a628
Adam Allam <adamyallam@gmail.com>
2024-08-16 15:09:44 -0500

    component cleanup

--- src/components/dashboard/pages/tasks/MyTasks.tsx
+++ src/components/dashboard/pages/tasks/MyTasks.tsx
@@ -1,18 +1,13 @@
 'use client'
-import { Plus, Filter, ArrowUpDown, ChevronDown } from "lucide-react"
-import { useContext } from "react";
+import { Plus, ChevronDown } from "lucide-react"
 
 // Component Imports
-import { OpenContext } from '@/components/context/OpenContext';
-import Transition from "../../Transition"
 
 export const MyTasks = () => { 
-  const isOpen = useContext(OpenContext);
-  
 
   return (
     <div>
-      <div className="flex gap-2 ml-8 mt-4">
+      <div className="flex gap-2 ml-8 mt-4 mb-4">
         <button><ChevronDown size={16} className=""/></button>
         <h1>My Tasks</h1>
         <button className=""><Plus size={16}/></button>
