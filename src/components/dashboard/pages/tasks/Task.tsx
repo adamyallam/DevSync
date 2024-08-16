a0f965b7d2ee3f9b3599425750ae0c4ff85dded0
Adam Allam <adamyallam@gmail.com>
2024-08-16 15:10:01 -0500

    added prop to determine if task should have a top border or not and cleaned up file

--- src/components/dashboard/pages/tasks/Task.tsx
+++ src/components/dashboard/pages/tasks/Task.tsx
@@ -1,22 +1,24 @@
 'use client'
-import { Plus, Filter, ArrowUpDown, ChevronDown, BadgeCheck, CalendarClock, Lock, UserRoundSearch } from "lucide-react"
+import { Plus, BadgeCheck, CalendarClock, Lock, UserRoundSearch } from "lucide-react"
 import { useContext, useState } from "react";
 
 // Component Imports
 import { OpenContext } from '@/components/context/OpenContext';
-import Transition from "../../Transition"
 
-export const Task = () => { 
+interface Props {
+  showTopBorder: boolean;
+}
+
+export const Task: React.FC<Props> = (props) => { 
   const isOpen = useContext(OpenContext);
   const [taskName, setTaskName] = useState('');
   const [projectSearch, setProjectSearch] = useState('')
 
   return (
     <div>
-      <Transition transition="translate-x-60">
-        <div className={`grid grid-cols-12 grid-rows-1 border-b-2 border-t-2 border-gray-300 ml-8 mt-4 h-10 transition-all duration-300 ${isOpen ? 'mr-72' : 'mr-8'}`}>
+        <div className={`grid grid-cols-12 grid-rows-1 border-b-2 border-gray-300 ml-8 h-10 transition-all duration-300 ${props.showTopBorder ? 'border-t-2' : ''} ${isOpen ? 'mr-72' : 'mr-8'}`}>
           <div className={`flex col-span-4 ml-2 border-r-2 border-gray-300}`}>
-            <button className="mr-1"><BadgeCheck size={22} color="green"/></button>
+            <button className="mr-1 ml-5"><BadgeCheck size={22} color="green"/></button>
             <input 
               type="text" 
               placeholder="Name"
@@ -41,7 +43,7 @@ export const Task = () => {
           </div>
 
           <div className={`flex items-center border-r-2 col-span-2`}>
-            <Lock size={22} className=""/>
+            <Lock size={22} className="ml-2"/>
             <p className="ml-1 text-sm">Only Me</p>
           </div>
 
@@ -50,7 +52,6 @@ export const Task = () => {
             <button className="pr-2"><Plus size={18}/></button>
           </div>
         </div>
-      </Transition>
     </div>
   )
 }
