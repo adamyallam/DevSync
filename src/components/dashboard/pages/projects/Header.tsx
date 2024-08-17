804324e01b6ebfc2f4ecd8197c667a91dd8c4c79
Adam Allam <adamyallam@gmail.com>
2024-08-17 17:28:56 -0500

    Added project name input field and icons for future name adjustments

--- src/components/dashboard/pages/projects/Header.tsx
+++ src/components/dashboard/pages/projects/Header.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useState } from "react"
+import { ChevronDown, Star } from "lucide-react"
 
 // Component Imports
 
@@ -8,14 +9,17 @@ export const Header = () => {
 
   return (
     <div>
-      <div className="flex">
+      <div className="flex gap-2 mt-16">
+        <div className="border-2 border-red-500 bg-red-300 w-8 h-8 rounded-xl ml-8" />
         <input 
         type='text'
         placeholder="Name"
         value={projectName}
-        onChange={(e) => setProjectName(e.target.value)} 
+        onChange={(e) => setProjectName(e.target.value)}
+        className="text-lg w-36" 
         />
-        <div className="border-2 border-red-500 bg-red-300 w-14 h-14 rounded-xl ml-5" />
+        <button><ChevronDown strokeWidth={2} size={20}/></button>
+        <button><Star strokeWidth={1} size={20}/></button>
       </div>
     </div>
   )
