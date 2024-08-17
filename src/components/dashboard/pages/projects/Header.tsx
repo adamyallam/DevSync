f285edf1ac126f0107d0682ed8eef91da8fc345a
Adam Allam <adamyallam@gmail.com>
2024-08-17 17:15:30 -0500

    started on header for project tabs

--- src/components/dashboard/pages/projects/Header.tsx
+++ src/components/dashboard/pages/projects/Header.tsx
@@ -1,14 +1,24 @@
 'use client'
+import { useState } from "react"
 
 // Component Imports
 
 export const Header = () => { 
+  const [projectName, setProjectName] = useState('')
 
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <div className="flex">
+        <input 
+        type='text'
+        placeholder="Name"
+        value={projectName}
+        onChange={(e) => setProjectName(e.target.value)} 
+        />
+        <div className="border-2 border-red-500 bg-red-300 w-14 h-14 rounded-xl ml-5" />
+      </div>
+    </div>
+  )
 }
 
 export default Header
