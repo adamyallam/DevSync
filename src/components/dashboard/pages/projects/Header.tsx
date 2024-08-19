13716921a1b23926abc6085f798467f1679e1971
Adam Allam <adamyallam@gmail.com>
2024-08-19 14:48:31 -0500

    finished project name input and top left of header

--- src/components/dashboard/pages/projects/Header.tsx
+++ src/components/dashboard/pages/projects/Header.tsx
@@ -1,11 +1,21 @@
 'use client'
-import { useState } from "react"
+import { useState, useEffect, useRef} from "react"
 import { ChevronDown, Star } from "lucide-react"
 
 // Component Imports
 
 export const Header = () => { 
   const [projectName, setProjectName] = useState('')
+  const inputRef = useRef<HTMLInputElement>(null);
+  const spanRef = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current && spanRef.current && spanRef.current.offsetWidth >= 125) {
+      inputRef.current.style.width = `${spanRef.current.offsetWidth + 2}px`;
+    } else if (inputRef.current) {
+      inputRef.current.style.width = '125px'
+    }
+  }, [projectName]);
 
   return (
     <div>
@@ -16,10 +26,23 @@ export const Header = () => {
         placeholder="Name"
         value={projectName}
         onChange={(e) => setProjectName(e.target.value)}
-        className="text-lg w-36" 
+        ref={inputRef}
+        className={`border border-gray-300 rounded px-2 py-1`}
+        style={{ width: '125px'}}
         />
+        <span
+          ref={spanRef}
+          className="absolute top-0 left-0 invisible whitespace-pre pr-5"
+        >
+          {projectName}
+        </span>
         <button><ChevronDown strokeWidth={2} size={20}/></button>
         <button><Star strokeWidth={1} size={20}/></button>
+        <button className="flex items-center gap-1 ml-3"><div className="border rounded-full border-black w-3 h-3"/>Set Status<ChevronDown strokeWidth={2} size={20}/></button>
+      </div>
+
+      <div>
+        
       </div>
     </div>
   )
