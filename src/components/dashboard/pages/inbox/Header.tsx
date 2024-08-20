f90619ff194eec31fb9dc4f114618b328d2b166b
Adam Allam <adamyallam@gmail.com>
2024-08-20 17:35:32 -0500

    added improved menu select underlining to each header

--- src/components/dashboard/pages/inbox/Header.tsx
+++ src/components/dashboard/pages/inbox/Header.tsx
@@ -11,15 +11,14 @@ export const Header = () => {
     return (
       <div>
         <Transition transition="translate-x-60">
-          <div className="overflow-hidden">
-            <div className="flex gap-4 ml-8 mt-1 text-sm">
-              <button>Activity<div className="border-b-2 border-black mt-0.5 outline outline-2 outline-black" /></button>
-              <button>Archive<div className="border-b-2 border-black mt-0.5 outline outline-2 outline-black" /></button>
-            </div>
-
-            <div className="border-b-2 -mt-0.5"/>
+          <div className="flex gap-4 ml-8 mt-1 text-sm">
+            <button className="z-10">Activity<div className="bg-black w-full h-[2.5px]"/></button>
+            <button className="z-10">Archive<div className="bg-black w-full h-[2.5px]"/></button>
           </div>
+
+          <div className="bg-gray-200 w-full h-[1.5px] z-0 -translate-y-[1.5px]" />
         </Transition>
+
         <div className="flex justify-between border-b-2 p-4">
           <Transition transition="translate-x-60">
             <button className="flex items-center gap-1 text-sm ml-7"><Filter size={20} strokeWidth={1}/>Filter</button>
--- src/components/dashboard/pages/tasks/Header.tsx
+++ src/components/dashboard/pages/tasks/Header.tsx
@@ -7,21 +7,22 @@ import { ChevronDown } from "lucide-react"
 export const Header = () => { 
 
     return (
-      <div className="border-b-2">
+      <div>
         <div className="flex items-center mb-2"> 
-          <div className="w-12 h-12 ml-8 border-2 border-gray-600 bg-gray-200 rounded-full"></div>
+          <div className="w-12 h-12 ml-8 border-2 border-gray-600 bg-gray-200 rounded-full" />
           <div className="ml-4">
             <div className="flex items-center gap-1">
               <h1 className="text-xl">My Tasks</h1>
               <ChevronDown size={20} strokeWidth={2} className="mt-1"/>
             </div>
             <div className="flex gap-5">
-              <button>List</button>
-              <button>Calendar</button>
-              <button>Files</button>
+              <button className="z-10">List<div className="bg-black w-full h-[2.5px] translate-y-[9.5px]"/></button>
+              <button className="z-10">Calendar<div className="bg-black w-full h-[2.5px] translate-y-[9.5px]"/></button>
+              <button className="z-10">Files<div className="bg-black w-full h-[2.5px] translate-y-[9.5px]"/></button>
             </div>
           </div>
         </div>
+        <div className="bg-gray-300 w-full h-[1.5px] z-0" />
       </div>
     )
 }
