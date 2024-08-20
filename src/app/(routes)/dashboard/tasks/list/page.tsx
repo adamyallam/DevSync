0baa8e1aa85ff2364b5d354935d88610ff616bcc
Adam Allam <adamyallam@gmail.com>
2024-08-20 17:58:06 -0500

    added list folder to store the "list" tab under the dashboard/tasks

---
+++ src/app/(routes)/dashboard/tasks/list/page.tsx
@@ -0,0 +1,25 @@
+
+// Component Imports
+import Header from '@/components/dashboard/pages/tasks/Header'
+import Transition from '@/components/dashboard/Transition'
+import TaskManagement from '@/components/dashboard/pages/tasks/TaskManagement'
+import MyTasks from '@/components/dashboard/pages/tasks/MyTasks'
+import Task from '@/components/dashboard/pages/tasks/Task'
+
+export default async function list() {
+
+  return (
+    <div>
+      <TaskManagement />
+      <Transition transition='translate-x-60'>
+        <MyTasks />
+        <Task showTopBorder={true} showAddTask={false}/>
+        <Task showTopBorder={false} showAddTask={false}/>
+        <Task showTopBorder={false} showAddTask={false}/>
+        <Task showTopBorder={false} showAddTask={true}/>
+      </Transition>
+      <h1 className='mt-20'>Making space</h1>
+    </div>
+  )
+
+}
