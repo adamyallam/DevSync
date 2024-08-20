40e5a253d954d6217d02cac0d499e32128ce6d46
Adam Allam <adamyallam@gmail.com>
2024-08-20 17:19:38 -0500

    Transition component can now be assigned optional additional classes

--- src/components/dashboard/Transition.tsx
+++ src/components/dashboard/Transition.tsx
@@ -7,13 +7,14 @@ import { OpenContext } from '@/components/context/OpenContext';
 export interface Props {
     children: React.ReactNode;
     transition: string
+    classes?: string
   }
 
   export const Transition: React.FC<Props> = (props) => { 
     const isOpen = useContext(OpenContext);
 
     return (
-        <div className={`transition-all duration-300 ${isOpen ? `${props.transition}` : ''}`}>
+        <div className={`transition-all duration-300 ${props.classes} ${isOpen ? `${props.transition}` : ''}`}>
             {props.children}
         </div>
     )
