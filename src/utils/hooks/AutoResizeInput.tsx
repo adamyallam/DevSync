05a900908c25df4a2fdc3b51fde271a0a53323ad
Adam Allam <adamyallam@gmail.com>
2024-08-28 12:08:02 -0500

    Added an auto resizing input field component that returns an input field the resizes as a user types

--- src/utils/hooks/AutoResizeInput.tsx
+++ src/utils/hooks/AutoResizeInput.tsx
@@ -1,90 +1,41 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-interface InputProps {
-  inputRef: React.RefObject<HTMLInputElement>;
-  spanRef: React.RefObject<HTMLSpanElement>;
-  minWidth: number;
-  initialState?: string;
-  inputClasses?: string;
-  spanClasses?: string;
+interface AutoResizeInputWidthProps {
+  initialWidth: number;
   placeholder?: string;
+  className?: string;
 }
 
-const AutoResizeInput: React.FC<InputProps> =  ({inputRef, spanRef, minWidth, initialState, inputClasses, spanClasses, placeholder}) => {
-
-  const [text, setText] = useState(initialState || '')
+const AutoResizingInput: React.FC<AutoResizeInputWidthProps> = ({initialWidth, placeholder, className,}) => {
+  const [text, setText] = useState(`${placeholder}`)
+  const inputRef = useRef<HTMLInputElement>(null);
+  const spanRef = useRef<HTMLSpanElement>(null);
 
   useEffect(() => {
-    if (inputRef.current && spanRef.current && spanRef.current.offsetWidth >= minWidth) {
+    if (inputRef.current && spanRef.current && spanRef.current.offsetWidth >= initialWidth) {
       inputRef.current.style.width = `${spanRef.current.offsetWidth + 2}px`;
     } else if (inputRef.current) {
-      inputRef.current.style.width = `${minWidth}px`
+      inputRef.current.style.width = `${initialWidth}px`;
     }
   }, [text]);
 
   return (
-    <div>
-      <div>
-        <input 
-        type='text'
+    <div className="relative">
+      <input
+        type="text"
         placeholder={placeholder}
         value={text}
         onChange={(e) => setText(e.target.value)}
         ref={inputRef}
-        className={inputClasses || `px-1 py-1 text-xl`}
-        style={{ width: `${minWidth}px`}} />
+        className={`${className ? `${className}` : 'px-1 py-1 text-md'}`}
+        style={{ width: `${initialWidth}px` }}>
+      </input>
 
-        <span
-          ref={spanRef}
-          className={`absolute top-0 left-0 invisible whitespace-pre pr-5 ${spanClasses || 'text-xl'}`}>
-          {text}
-        </span>
-      </div>
+      <span ref={spanRef} className={`${className ? `absolute top-0 left-0 invisible whitespace-pre pr-2 ${className}` : 'absolute top-0 left-0 invisible whitespace-pre pr-5 text-md'}`}>
+        {text}
+      </span>
     </div>
   );
 };
 
-export default AutoResizeInput;
-
-
-// interface AutoResizeInputProps {
-//   inputRef: React.RefObject<HTMLInputElement>;
-//   spanRef: React.RefObject<HTMLSpanElement>;
-//   stateValue: string;
-//   setStateValue: React.Dispatch<React.SetStateAction<string>>;
-//   minWidth: number;
-// }
-
-// const AutoResizeInput: React.FC<AutoResizeInputProps> = ({ inputRef, spanRef, stateValue, minWidth }) => {
-//   useEffect(() => {
-//     if (inputRef.current && spanRef.current && spanRef.current.offsetWidth >= minWidth) {
-//       inputRef.current.style.width = `${spanRef.current.offsetWidth + 2}px`;
-//     } else if (inputRef.current) {
-//       inputRef.current.style.width = `${minWidth}px`;
-//     }
-//   }, [stateValue, inputRef, spanRef, minWidth]);
-
-//   return null;
-// };
-
-// export default AutoResizeInput;
-
-
-
-
-// import { useState, useEffect } from 'react';
-
-// const useIncreasingInput = (inputRef, spanRef, state1, state2) => {
-//   // const [state1, state2] = useState<string>('');
-
-//   useEffect(() => {
-//     if (inputRef.current && spanRef.current && spanRef.current.offsetWidth >= 200) {
-//       inputRef.current.style.width = `${spanRef.current.offsetWidth + 2}px`;
-//     } else if (inputRef.current) {
-//       inputRef.current.style.width = '200px'
-//     }
-//   }, []);
-
-// };
-
-// export default useIncreasingInput;
+export default AutoResizingInput;
