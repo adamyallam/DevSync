afa4c1c60d855b1c0596efb3574e8c566de1ed11
Adam Allam <adamyallam@gmail.com>
2024-08-28 12:59:28 -0500

    fully implemented the resizing input component into Header and ProjectGreeting of the Projects page

--- src/components/dashboard/pages/projects/Header.tsx
+++ src/components/dashboard/pages/projects/Header.tsx
@@ -4,42 +4,16 @@ import { ChevronDown, Star, Ellipsis, PanelsTopLeft, ListOrdered, SquareKanban, 
 
 // Component Imports
 import Transition from "../../Transition"
+import AutoResizingInput from "@/components/styledElements/AutoResizeInput"
 
 export const Header = () => { 
-  const [projectName, setProjectName] = useState('')
-  const inputRef = useRef<HTMLInputElement>(null);
-  const spanRef = useRef<HTMLSpanElement>(null);
-
-  useEffect(() => {
-    if (inputRef.current && spanRef.current && spanRef.current.offsetWidth >= 125) {
-      inputRef.current.style.width = `${spanRef.current.offsetWidth + 2}px`;
-    } else if (inputRef.current) {
-      inputRef.current.style.width = '125px'
-    }
-  }, [projectName]);
 
   return (
     <div>
       <div className="flex">
         <Transition classes='flex gap-2 mt-16 w-full' transition="translate-x-60">
           <div className="border-2 border-red-500 bg-red-300 w-8 h-8 rounded-xl ml-8" />
-          <input 
-          type='text'
-          placeholder="Name"
-          value={projectName}
-          onChange={(e) => setProjectName(e.target.value)}
-          ref={inputRef}
-          className={`border border-gray-300 rounded px-2 py-1`}
-          style={{ width: '125px'}}
-          />
-
-          <span
-            ref={spanRef}
-            className="absolute top-0 left-0 invisible whitespace-pre pr-5"
-          >
-            {projectName}
-          </span>
-
+          <AutoResizingInput className="border border-gray-300 rounded px-2 py-1" initialWidth={125} initialState="Project Name" />
           <button><ChevronDown strokeWidth={2} size={20}/></button>
           <button><Star strokeWidth={1} size={20}/></button>
           <button className="flex items-center gap-1 ml-3"><div className="border rounded-full border-black w-3 h-3"/>Set Status<ChevronDown strokeWidth={2} size={20}/></button>
--- src/components/dashboard/pages/projects/overview/ProjectGreeting.tsx
+++ src/components/dashboard/pages/projects/overview/ProjectGreeting.tsx
@@ -11,7 +11,7 @@ export const ProjectGreeting = () => {
   return (
     <div>
       <Transition classes='flex flex-col ml-20 mt-12' transition="translate-x-60">
-        <AutoResizingInput initialWidth={200} placeholder="Project Greeting..." className="text-xl px-1 py-1"/>
+        <AutoResizingInput initialWidth={200} placeholder="Project Greeting..." className="text-xl px-1 py-1" initialState="Project Greeting..."/>
         <textarea
         className="border-2 border-gray-300 rounded w-1/2" 
         rows={5}
