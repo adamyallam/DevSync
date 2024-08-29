8f5ff0a614f5c08e237aff5a38d564b6ca689786
Adam Allam <adamyallam@gmail.com>
2024-08-29 12:16:25 -0500

    Changed placeholder and initial state on autoResizingInput component

--- src/components/dashboard/pages/projects/overview/ProjectOverview.tsx
+++ src/components/dashboard/pages/projects/overview/ProjectOverview.tsx
@@ -11,7 +11,7 @@ export const ProjectOverview = () => {
   return (
     <div>
       <Transition classes='flex flex-col ml-20 mt-12' transition="translate-x-60">
-        <AutoResizingInput initialWidth={200} placeholder="Project Greeting..." className="text-xl px-1 py-1" initialState="Project Greeting..."/>
+        <AutoResizingInput initialWidth={200} placeholder="Project Description..." className="text-xl px-1 py-1" initialState="Project Description..."/>
         <textarea
         className="border-2 border-gray-300 rounded w-1/2 resize-none px-2 mt-1" 
         rows={7}
