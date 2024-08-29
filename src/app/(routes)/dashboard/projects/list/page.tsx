1e114b5ef6d2afd267dc91df98fca54fd10d88bb
Adam Allam <adamyallam@gmail.com>
2024-08-29 12:16:56 -0500

    Created list parent component

---
+++ src/app/(routes)/dashboard/projects/list/page.tsx
@@ -0,0 +1,15 @@
+
+// Component Imports
+import ProjectOverview from "@/components/dashboard/pages/projects/overview/ProjectOverview"
+import Transition from "@/components/dashboard/Transition"
+
+export default async function overview() {
+
+  return (
+    <div>
+      
+      <div className="mt-56">making space</div>
+    </div>
+  )
+
+}
