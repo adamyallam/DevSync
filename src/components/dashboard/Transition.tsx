a5487dce2c1274e4dcbba19c817ac224a9d035cd
Adam Allam <adamyallam@gmail.com>
2024-09-03 12:20:47 -0500

    indenting

--- src/components/dashboard/Transition.tsx
+++ src/components/dashboard/Transition.tsx
@@ -8,16 +8,16 @@ export interface Props {
     children: React.ReactNode;
     transition: string
     classes?: string
-  }
+}
 
-  export const Transition: React.FC<Props> = (props) => { 
-    const isOpen = useContext(SidebarUIContext);
+export const Transition: React.FC<Props> = (props) => { 
+  const isOpen = useContext(SidebarUIContext);
 
-    return (
-        <div className={`transition-all duration-300 ${props.classes} ${isOpen ? `${props.transition}` : ''}`}>
-            {props.children}
-        </div>
-    )
+  return (
+    <div className={`transition-all duration-300 ${props.classes} ${isOpen ? `${props.transition}` : ''}`}>
+        {props.children}
+    </div>
+  )
 }
 
 export default Transition
