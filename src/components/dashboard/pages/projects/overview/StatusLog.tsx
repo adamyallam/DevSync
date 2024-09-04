b0a309402dee78c2791da255fca79ef59b7ed5c9
Adam Allam <adamyallam@gmail.com>
2024-09-04 12:59:30 -0500

    adjusted element styling

--- src/components/dashboard/pages/projects/overview/StatusLog.tsx
+++ src/components/dashboard/pages/projects/overview/StatusLog.tsx
@@ -3,23 +3,23 @@ import { Calendar } from "lucide-react"
 //Component imports
 
 export const StatusLog = () => {
+
   return (
-    <div className="bg-gray-100 h-screen">
-      <div>
-        <div className="pt-5 ml-8">
-          <h1 className="text-xl">Project status:</h1>
+    <div className="h-screen hborder border-gray-200 bg-gray-100 p-4 -mt-[2px]"> {/** mt[132px] */}
+      <h1 className="text-xl mb-4">Project status:</h1>
 
-          <div className="flex gap-4 mt-3">
-            <button className="flex items-center gap-1 border border-gray-400 bg-white p-1 rounded-md text-sm"><div className="border-2 border-green-500 bg-green-500 w-2 h-2 rounded-full"/>On track</button>
-            <button className="flex items-center gap-1 border border-gray-400 bg-white p-1 rounded-md text-sm"><div className="border-2 border-yellow-500 bg-yellow-500 w-2 h-2 rounded-full"/>At risk</button>
-            <button className="flex items-center gap-1 border border-gray-400 bg-white p-1 rounded-md text-sm"><div className="border-2 border-red-500 bg-red-500 w-2 h-2 rounded-full"/>Off track</button>
-          </div>
-        </div>
-
-        <div className="mt-10 ml-7">
-          <button className="flex items-center gap-1"><div className="flex items-center justify-center border-2 border-dashed border-gray-300 rounded-full w-9 h-9"><Calendar size={18} color="gray"/></div>Due date</button>
-        </div>
+      <div className="flex gap-4 mt-3">
+        <button className="flex items-center justify-center w-20 gap-1 border border-gray-400 bg-white p-1 rounded-md text-sm"><div className="border-2 border-green-500 bg-green-500 w-2 h-2 rounded-full"/>On track</button>
+        <button className="flex items-center justify-center w-20  gap-1 border border-gray-400 bg-white p-1 rounded-md text-sm"><div className="border-2 border-yellow-500 bg-yellow-500 w-2 h-2 rounded-full"/>At risk</button>
+        <button className="flex items-center justify-center w-20 gap-1 border border-gray-400 bg-white p-1 rounded-md text-sm"><div className="border-2 border-red-500 bg-red-500 w-2 h-2 rounded-full"/>Off track</button>
       </div>
+
+      <button className="flex items-center gap-2 mt-5">
+        <div className="flex justify-center items-center border border-dashed rounded-full border-black w-8 h-8">
+          <Calendar size={17} />
+        </div>
+        No Due Date
+      </button>
     </div>
   )
 }
