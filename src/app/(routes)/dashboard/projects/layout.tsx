f98a98a9701c213cfbacfca1ff0d6436c71e588b
Adam Allam <adamyallam@gmail.com>
2024-09-10 21:37:14 -0500

    Added Navbar to layout file

--- src/app/(routes)/dashboard/projects/layout.tsx
+++ src/app/(routes)/dashboard/projects/layout.tsx
@@ -1,6 +1,7 @@
 
 //Component Imports
 import Header from "@/components/dashboard/pages/projects/Header"
+import Navbar from "@/components/dashboard/Navbar"
 
 interface Props {
   children: React.ReactNode
@@ -9,6 +10,7 @@ interface Props {
 export const dashboardLayout: React.FC<Props> = ({children}) => {
     return (
       <section className='overflow-x-hidden'>
+        <Navbar />
         <Header />
         {children}
       </section>
