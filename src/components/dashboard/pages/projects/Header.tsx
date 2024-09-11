2b29ef30dd6fc9b9884d73518652356ee945957c
Adam Allam <adamyallam@gmail.com>
2024-09-10 21:43:44 -0500

    removed all mt classes

--- src/components/dashboard/pages/projects/Header.tsx
+++ src/components/dashboard/pages/projects/Header.tsx
@@ -11,7 +11,7 @@ export const Header = () => {
   return (
     <div>
       <div className="flex">
-        <Transition classes='flex gap-2 mt-16 w-full' transition="translate-x-60">
+        <Transition classes='flex gap-2 w-full' transition="translate-x-60">
           <div className="border-2 border-red-500 bg-red-300 w-8 h-8 rounded-xl ml-8" />
           <AutoResizingInput className="border border-gray-300 rounded px-2 py-1" initialWidth={125} initialState="Project Name" />
           <button><ChevronDown strokeWidth={2} size={20}/></button>
@@ -19,7 +19,7 @@ export const Header = () => {
           <button className="flex items-center gap-1 ml-3"><div className="border rounded-full border-black w-3 h-3"/>Set Status<ChevronDown strokeWidth={2} size={20}/></button>
         </Transition>
 
-        <div className="flex gap-2 items-center justify-end mt-16 mr-8">
+        <div className="flex gap-2 items-center justify-end mr-8">
           <button className="flex">
             <div className="border rounded-full border-red-600 bg-red-400 w-7 h-7 translate-x-1.5 outline outline-2 outline-white"/>
             <div className="flex items-center justify-center border rounded-full border-gray-300 bg-gray-100 w-7 h-7"><Ellipsis color="#6b6b6b" size={17} strokeWidth={3} /></div>
@@ -30,7 +30,7 @@ export const Header = () => {
       </div>
 
       <Transition transition="translate-x-60">
-        <div className="flex gap-4 ml-10 mt-2">
+        <div className="flex gap-4 ml-10 ">
           <Link href={'/dashboard/projects/overview'} className="z-10"><div className="flex items-center gap-1"><PanelsTopLeft size={14}/>Overview</div> <div className="bg-black w-full h-[2.5px]"/></Link>
           <Link href={'/dashboard/projects/list'} className="z-10"><div className="flex items-center gap-1"><ListOrdered size={16}/>List</div> <div className="bg-black w-full h-[2.5px]"/></Link>
           <Link href={'/dashboard/projects/board'} className="z-10"><div className="flex items-center gap-1"><SquareKanban size={14}/>Board</div> <div className="bg-black w-full h-[2.5px]"/></Link>
