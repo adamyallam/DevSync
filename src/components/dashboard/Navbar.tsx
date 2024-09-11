45099c52a3438754bbf1c46afeaf6cbb598f2773
Adam Allam <adamyallam@gmail.com>
2024-09-11 14:36:04 -0500

    Added sidebar scroll feature

--- src/components/dashboard/Navbar.tsx
+++ src/components/dashboard/Navbar.tsx
@@ -33,7 +33,7 @@ export const Navbar = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.innerHeight <= 540 ) {
+      if (window.innerHeight <= 510 ) {
         setIsFixed(false)
       } else {
         setIsFixed(true)
@@ -62,27 +62,26 @@ export const Navbar = () => {
           <button className='border bg-white rounded-full w-8 h-8 mr-2'>PFP</button>
       </div>
 
-      <div className={`fixed left-0 bottom-0 top-12 text-gray-300 bg-gray-700 transition-transform duration-300 ${isSidebarOpen ? 'translate-x-0' : '-translate-x-60'}`}>
-        <div className='bg-gray-700 w-60'>
-          <div className='border-t-2 border-b-2 border-gray-600 pt-4 pb-4'>
-            <Link href='/dashboard/home' className={`flex items-center h-8 ${applySidebarClass('dashboard/home')}`}>
-              <Home size={20} color="#e5e7eb" strokeWidth={1.5}/>
-              <span className='ml-1 text-sm'>Home</span>
-            </Link>
-            <Link href='/dashboard/tasks/list' className={`flex items-center h-8 ${applySidebarClass('tasks/list', 'tasks/calendar', 'tasks/files')}`}>
-              <CircleCheck size={20} color="#e5e7eb" strokeWidth={1.5}/>
-              <span className='ml-1 text-sm'>Tasks</span>
-            </Link>
+      <div className={`fixed left-0 bottom-0 top-12 text-gray-300 bg-gray-700 w-60 transition-transform duration-300 ${isSidebarOpen ? 'translate-x-0' : '-translate-x-60'} overflow-x-hidden overflow-y-hidden hover:overflow-y-auto`}>
+        <div className='border-t-2 border-b-2 border-gray-600 pt-4 pb-4'>
+          <Link href='/dashboard/home' className={`flex items-center h-8 ${applySidebarClass('dashboard/home')}`}>
+            <Home size={20} color="#e5e7eb" strokeWidth={1.5}/>
+            <span className='ml-1 text-sm'>Home</span>
+          </Link>
           
-            <Link href='/dashboard/inbox/activity' className={`flex items-center h-8 ${applySidebarClass('inbox/activity', 'inbox/archive')}`} >
-              <Inbox size={20} color="#e5e7eb" strokeWidth={1.5}/>
-              <span className='ml-1 text-sm'>Inbox</span>
-            </Link>
-          </div>
+          <Link href='/dashboard/tasks/list' className={`flex items-center h-8 ${applySidebarClass('tasks/list', 'tasks/calendar', 'tasks/files')}`}>
+            <CircleCheck size={20} color="#e5e7eb" strokeWidth={1.5}/>
+            <span className='ml-1 text-sm'>Tasks</span>
+          </Link>
+        
+          <Link href='/dashboard/inbox/activity' className={`flex items-center h-8 ${applySidebarClass('inbox/activity', 'inbox/archive')}`} >
+            <Inbox size={20} color="#e5e7eb" strokeWidth={1.5}/>
+            <span className='ml-1 text-sm'>Inbox</span>
+          </Link>
         </div>
 
-        <div className='bg-gray-700 w-60 h-64 overflow-y-hidden hover:overflow-y-auto overflow-x-hidden'>
-          <div className='pb-5 pt-5'>
+        <div>
+          <div className='pb-3 pt-3'>
             <h1 className='ml-8 font-bold'>Insights</h1>
             <Link href='/dashboard/calendar' className={`flex items-center h-8 ${applySidebarClass('dashboard/calendar')}`}>
               <CalendarCheck size={20} color="#e5e7eb" strokeWidth={1.5}/>
@@ -90,15 +89,15 @@ export const Navbar = () => {
             </Link>
           </div>
           
-          <div className='pb-5'>
+          <div className='pb-3'>
             <h1 className='ml-8 font-bold'>Projects</h1>
             <Link href='/dashboard/projects/overview' className={`flex items-center h-8 mb-1 ${applySidebarClass('projects/overview', 'projects/list', 'projects/board', 'projects/calendar', 'projects/files')}`}>
               <div className='border-2 bg-white rounded-md w-4 h-4' />
               <span className='ml-2 text-sm'>ProjectName</span>
-            </Link>   
+            </Link>
           </div>
 
-          <div>
+          <div className='pb-3'>
             <h1 className='ml-8 font-bold'>Team</h1>
             <Link href='/dashboard/workspace' className={`flex items-center h-8 ${applySidebarClass('dashboard/workspace')}`}>
               <UserRound size={20} color="#e5e7eb" strokeWidth={1.5}/>
@@ -108,7 +107,7 @@ export const Navbar = () => {
         </div>
       </div>
 
-      <div className={`bg-gray-700 w-60 text-gray-200 ${isFixed ? 'fixed bottom-0' : 'fixed top-[435px]'} transition-all duration-300 ${isSidebarOpen ? '' : '-translate-x-60'}`}>
+      <div className={`bg-gray-700 w-60 text-gray-200 ${isFixed ? 'fixed bottom-0' : 'fixed top-[405px]'} transition-all duration-300 ${isSidebarOpen ? '' : '-translate-x-60'}`}>
         <div className='flex flex-col items-center border-t-2 pb-3'>
           <button className='border-2 p-2 w-11/12 mt-4'>Create Project</button>
           <div className='flex mt-2 gap-2'>
