fcfc5ab0814d131e756c56c8bbd17c7efcd36fae
Adam Allam <adamyallam@gmail.com>
2024-09-16 16:09:33 -0500

    Updated bg and border colors

--- src/app/(routes)/dashboard/projects/board/page.tsx
+++ src/app/(routes)/dashboard/projects/board/page.tsx
@@ -7,8 +7,8 @@ export default async function board() {
   return (
     <div className="flex flex-col w-full h-full">
       <AddTaskButton />
-      <div className="overflow-auto bg-gray-100 w-full h-full">
-
+      <div className="overflow-auto bg-gray-50 w-full h-full border-t-2 border-gray-100">
+      
       </div>
     </div>
   )
