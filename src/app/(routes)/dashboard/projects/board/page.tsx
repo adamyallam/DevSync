18bebd4ac3f4cdee0b3700fcfad12a8d31e5eb60
Adam Allam <adamyallam@gmail.com>
2024-09-16 15:37:34 -0500

    added starting classes

--- src/app/(routes)/dashboard/projects/board/page.tsx
+++ src/app/(routes)/dashboard/projects/board/page.tsx
@@ -5,8 +5,11 @@ import AddTaskButton from "@/components/styledElements/AddTaskButton"
 export default async function board() {
 
   return (
-    <div className="w-full h-full">
+    <div className="flex flex-col w-full h-full">
       <AddTaskButton />
+      <div className="overflow-auto bg-gray-100 w-full h-full">
+
+      </div>
     </div>
   )
 
