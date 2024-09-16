7feafb090feeb17ee5f69c6749defd989e66f7ef
Adam Allam <adamyallam@gmail.com>
2024-09-16 13:14:42 -0500

    created task component for projects route

---
+++ src/components/dashboard/pages/projects/list/Task.tsx
@@ -0,0 +1,58 @@
+'use client'
+import { Plus, BadgeCheck, CalendarClock, ChevronDown, UserRoundSearch } from "lucide-react"
+import { useState } from "react";
+import useScreenWidth from "@/utils/hooks/useScreenWidth";
+import useNavbarUIContext from "@/utils/hooks/useNavbarUIContext";
+
+// Component Imports
+
+
+interface Props {
+  showTopBorder: boolean;
+  showAddTask: boolean;
+}
+
+export const Task: React.FC<Props> = (props) => { 
+  const { isSidebarOpen } = useNavbarUIContext();
+
+  const [taskName, setTaskName] = useState('');
+  const [projectSearch, setProjectSearch] = useState('')
+  const screenWidth = useScreenWidth()
+
+  return (
+    <div>
+      <div className={`grid grid-cols-10 grid-rows-1 border-b-2 border-gray-300 ml-8 h-10 transition-all duration-300 ${props.showTopBorder ? 'border-t-2' : ''}`} 
+      style={{width: isSidebarOpen ? `${screenWidth - 304}px` : `${screenWidth - 64}px`}}>
+        <div className={`flex col-span-4 ml-2 border-r-2`}>
+          <button className="mr-1 ml-5"><BadgeCheck size={22} color="green"/></button>
+          <input 
+            type="text" 
+            placeholder="Name"
+            value={taskName} 
+            onChange={(e) => setTaskName(e.target.value)} 
+            className="text-sm w-full"
+          />
+        </div>
+
+        <div className={`flex items-center justify-between border-r-2 col-span-2`}>
+          <button className="w-full pl-2"><UserRoundSearch size={22}/></button>
+          <button className="pr-2"><Plus size={18}/></button>
+        </div>
+
+        <div className={`flex border-r-2 col-span-2`}>
+          <button className="w-full pl-2"><CalendarClock size={22} /></button>
+        </div>
+
+        <div className="self-center col-span-2">
+          <button className="flex items-center gap-1"><div className="border rounded-full bg-red-500 w-3 h-3 ml-2 mr-1"/>High<ChevronDown size={13} className="mt-1"/></button>
+        </div>
+      </div>
+
+      <div className={props.showAddTask ? '' : 'hidden'}>
+        <button className="ml-20 mt-2 text-sm">Add task...</button>
+      </div>
+    </div>
+  )
+}
+
+export default Task
