cf5079336dddb34a847208df9719adbe1359db8c
Adam Allam <adamyallam@gmail.com>
2024-09-16 16:52:31 -0500

    adjusted spacing classes

--- src/components/styledElements/AddTaskButton.tsx
+++ src/components/styledElements/AddTaskButton.tsx
@@ -4,15 +4,15 @@ import { Filter, ArrowUpDown, Plus } from "lucide-react";
 export const AddTaskButton = () => {
 
   return (
-    <div className="mb-4">
+    <div className="mb-2 mt-2">
       <div className="grid grid-cols-2">
-        <div className="ml-8 mt-4">
+        <div className="ml-8 ">
           <div>
             <button className="flex items-center gap-1 border-2 bg-blue-500 w-w-22 h-9 rounded-md p-1 text-sm text-white"><Plus size={16}/>Add task</button>
           </div>
         </div>
 
-        <div className="flex justify-end gap-5 mt-4 mr-10">
+        <div className="flex justify-end gap-5  mr-10">
           <button className="flex items-center gap-1"><Filter size={16}/>Filter</button>
           <button className="flex items-center gap-1"><ArrowUpDown size={16}/>Sort</button>
         </div>
