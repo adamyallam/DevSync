95ca1a06a1fc3a51786fecde243ef2ce49e98da6
Adam Allam <adamyallam@gmail.com>
2024-09-16 16:52:22 -0500

    adjusted spacing classes

--- src/components/dashboard/pages/TaskSection.tsx
+++ src/components/dashboard/pages/TaskSection.tsx
@@ -8,14 +8,14 @@ import AutoResizingInput from "@/components/styledElements/AutoResizingInput"
 export const TaskSection = () => {
 
   return (
-    <div className="h-full">
+    <div className="mt-6">
       <div className="flex gap-2 ml-8 mt-2 mb-2">
         <button><ChevronDown size={16} className=""/></button>
         <AutoResizingInput placeholder="Untitled Section"/>
         <button className=""><Plus size={16}/></button>
       </div>
 
-      <div className="pb-10">
+      <div>
         <ProjectTask showTopBorder={true} showAddTask={false}/>
         <ProjectTask showTopBorder={false} showAddTask={false}/>
         <ProjectTask showTopBorder={false} showAddTask={false}/>
