b5b31921d68632a6c8ee4615a9d33c6c900cffdd
Adam Allam <adamyallam@gmail.com>
2024-09-16 16:21:43 -0500

    Set Calendar and Workspace sections of the site to default (No code added to either pages)

--- src/app/(routes)/dashboard/calendar/page.tsx
+++ src/app/(routes)/dashboard/calendar/page.tsx
@@ -1,13 +1,13 @@
 
 // Component Imports
-import Calendar from '@/components/dashboard/pages/Calendar'
+
 
 
 export default function calendar(){
 
   return (
     <div>
-      <Calendar />
+
     </div>
   )
 
--- src/app/(routes)/dashboard/workspace/page.tsx
+++ src/app/(routes)/dashboard/workspace/page.tsx
@@ -1,12 +1,12 @@
 
 //Component Imports
-import Workspace from '@/components/dashboard/pages/Workspace'
+
 
 export default async function workspace(){
   
   return (
     <div>
-      <Workspace />
+
     </div>
   )
 
