7316d8bde2d7864d386e8ea4d87993cff65dea23
Adam Allam <adamyallam@gmail.com>
2024-09-17 14:49:09 -0500

    added functionality to "add task" button, when clicked now adds a task.

--- src/components/dashboard/pages/TaskSection.tsx
+++ src/components/dashboard/pages/TaskSection.tsx
@@ -1,11 +1,37 @@
 'use client'
 import { ChevronDown, Plus } from "lucide-react"
+import { usePathSegments } from "@/utils/hooks/usePathSegments"
+import { useState, useEffect } from "react"
 
 //component imports
 import ProjectTask from '@/components/dashboard/pages/projects/list/ProjectTask'
+import MyTask from "./tasks/list/MyTask"
 import AutoResizingInput from "@/components/styledElements/AutoResizingInput"
 
 export const TaskSection = () => {
+  const [tasks, setTasks] = useState<JSX.Element[]>([])
+  const [isFirstTask, setIsFirstTask] = useState<boolean>(false)
+
+  const currentPath = usePathSegments(2);
+
+  function addTask() {
+    const newTask = (() => {
+      if (currentPath === 'projects/list') {
+        return <ProjectTask key={tasks.length} showTopBorder={!isFirstTask} />
+      } else if (currentPath === 'tasks/list') {
+        return <MyTask key={tasks.length} showTopBorder={!isFirstTask} />
+      } else {
+        throw new Error('Invalid path for adding a task.')
+      }
+    })();
+
+    setTasks((prevTasks) => {
+      if (!isFirstTask) {
+        setIsFirstTask(true)
+      }
+      return [...prevTasks, newTask]
+    })
+  }
 
   return (
     <div className="mt-6">
@@ -16,23 +42,10 @@ export const TaskSection = () => {
       </div>
 
       <div>
-        <ProjectTask showTopBorder={true} />
-        <ProjectTask />
-        <ProjectTask />
-        <ProjectTask />
-        <ProjectTask />
-        <ProjectTask />
-        <ProjectTask />
-        <ProjectTask />
-        <ProjectTask />
-        <ProjectTask />
-        <ProjectTask />
-        <ProjectTask />
-        <ProjectTask />
-        <ProjectTask/>
+        {tasks}
       </div>
 
-      <button className="ml-12 mt-2 text-sm">Add task...</button>
+      <button className="ml-12 mt-2 text-sm" onClick={() => addTask()}>Add task...</button>
     </div>
   )
 }
