60ca720e813f72a3c3a320e134fbebd1ebc194dc
Adam Allam <adamyallam@gmail.com>
2024-09-18 15:08:35 -0500

    changed maxGrowthSize on input

--- src/components/dashboard/pages/BoardTaskSection.tsx
+++ src/components/dashboard/pages/BoardTaskSection.tsx
@@ -9,7 +9,7 @@ export const BoardTaskSection = () => {
   return (
     <div className="flex flex-col ml-8 mt-5 border rounded-lg border-gray-400 w-[270px] h-[calc(100%-40px)]">
       <div className="flex justify-between ml-2 mb-3 mt-2 mr-2">
-        <AutoResizingInput maxGrowthWidth={185} placeholder="Untitled Section"/>
+        <AutoResizingInput maxGrowthWidth={200} placeholder="Untitled Section"/>
 
         <div className="flex gap-2">
           <button><Plus size={20} /></button>
