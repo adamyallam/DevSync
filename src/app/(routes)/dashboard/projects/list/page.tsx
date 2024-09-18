86d7d37dd0af9e359a509a53ee5d30fe0c83f055
Adam Allam <adamyallam@gmail.com>
2024-09-17 22:16:42 -0500

    changed taskSection to CreateTaskSection component

--- src/app/(routes)/dashboard/projects/list/page.tsx
+++ src/app/(routes)/dashboard/projects/list/page.tsx
@@ -1,7 +1,7 @@
 
 // Component Imports
 import ProjectTasksManager from "@/components/dashboard/pages/projects/list/ProjectTasksManager"
-import TaskSection from "@/components/dashboard/pages/TaskSection"
+import CreateTaskSection from "@/components/dashboard/pages/CreateTaskSection"
 
 export default async function list() {
 
@@ -9,7 +9,7 @@ export default async function list() {
     <div className="flex flex-col h-full w-full">
       <ProjectTasksManager />
       <div className="overflow-x-hidden overflow-y-auto pb-10">
-        <TaskSection />
+        <CreateTaskSection />
       </div>
     </div>
   )
