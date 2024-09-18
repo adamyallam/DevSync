ed4f4548bd8e0385308bc277600e2426a2c98c47
Adam Allam <adamyallam@gmail.com>
2024-09-18 14:15:11 -0500

    Created component

---
+++ src/components/dashboard/pages/BoardTaskSection.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+//component imports
+
+export const BoardTaskSection = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default BoardTaskSection
---
+++ src/components/dashboard/pages/projects/board/ProjectBoardTask.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { BadgeCheck, User, CalendarClock } from "lucide-react"
+
+//component imports
+import AutoResizingInput from "@/components/styledElements/AutoResizingInput"
+
+export const ProjectBoardTask = () => {
+  
+  return (
+    <div>
+      <div className="flex flex-col justify-evenly border border-gray-200 bg-white rounded-lg w-60 h-24">
+        <div className="flex items-center gap-1 ml-2">
+          <BadgeCheck size={18} color="green"/>
+          <AutoResizingInput placeholder="Untitled Task" className="text-md"/>
+        </div>
+
+        <div className="flex gap-2 ml-2">
+          <div className="flex justify-center items-center border border-dashed rounded-full border-gray-500 w-6 h-6">
+            <User color="gray" size={18} />
+          </div>
+          <div className="flex justify-center items-center border border-dashed rounded-full border-gray-500 w-6 h-6">
+            <CalendarClock color="gray" size={16}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectBoardTask
