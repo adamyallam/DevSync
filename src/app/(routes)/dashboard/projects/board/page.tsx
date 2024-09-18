ae9f38f6218b8b82154d7b1bd0c0a198c55f8dd2
Adam Allam <adamyallam@gmail.com>
2024-09-18 14:14:12 -0500

    Added starter classes and components

--- src/app/(routes)/dashboard/projects/board/page.tsx
+++ src/app/(routes)/dashboard/projects/board/page.tsx
@@ -1,14 +1,15 @@
 
 // Component Imports
 import AddTaskButton from "@/components/styledElements/AddTaskButton"
+import ProjectBoardTask from "@/components/dashboard/pages/projects/board/ProjectBoardTask"
 
 export default async function board() {
 
   return (
     <div className="flex flex-col w-full h-full">
       <AddTaskButton />
-      <div className="overflow-auto bg-gray-50 w-full h-full border-t-2 border-gray-100">
-      
+      <div className="overflow-x-hidden overflow-y-auto w-full h-full bg-gray-50 border-t-2 border-gray-100">
+        <ProjectBoardTask />
       </div>
     </div>
   )
