4b6dafcb48c62d1d80a3e815271e25cd49970b74
Adam Allam <adamyallam@gmail.com>
2024-09-17 21:35:33 -0500

    Component now renders with a task

--- src/components/dashboard/pages/TaskSection.tsx
+++ src/components/dashboard/pages/TaskSection.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { ChevronDown, Plus } from "lucide-react"
 import { usePathSegments } from "@/utils/hooks/usePathSegments"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 //component imports
 import ProjectTask from '@/components/dashboard/pages/projects/list/ProjectTask'
@@ -9,27 +9,33 @@ import MyTask from "./tasks/list/MyTask"
 import AutoResizingInput from "@/components/styledElements/AutoResizingInput"
 
 export const TaskSection = () => {
-  const [tasks, setTasks] = useState<JSX.Element[]>([])
-  const [isFirstTask, setIsFirstTask] = useState<boolean>(false)
-
   const currentPath = usePathSegments(2);
+  const [isFirstTask, setIsFirstTask] = useState<boolean>(true)
+  const [tasks, setTasks] = useState<JSX.Element[]>([])
+
+  useEffect(() => {
+      setTasks(() => {
+    return [newTask()]
+  })
+  }, [])
+  
+  useEffect(() => {
+    setIsFirstTask(tasks.length === 0)
+  }, [tasks])
+
+  function newTask() {
+    if (currentPath === 'projects/list') {
+      return <ProjectTask key={tasks.length} showTopBorder={isFirstTask} />
+    } else if (currentPath === 'tasks/list') {
+      return <MyTask key={tasks.length} showTopBorder={isFirstTask} />
+    } else {
+      throw new Error('Invalid path for adding a task.')
+    }
+  };
 
   function addTask() {
-    const newTask = (() => {
-      if (currentPath === 'projects/list') {
-        return <ProjectTask key={tasks.length} showTopBorder={!isFirstTask} />
-      } else if (currentPath === 'tasks/list') {
-        return <MyTask key={tasks.length} showTopBorder={!isFirstTask} />
-      } else {
-        throw new Error('Invalid path for adding a task.')
-      }
-    })();
-
     setTasks((prevTasks) => {
-      if (!isFirstTask) {
-        setIsFirstTask(true)
-      }
-      return [...prevTasks, newTask]
+      return [...prevTasks, newTask()]
     })
   }
 
@@ -45,7 +51,7 @@ export const TaskSection = () => {
         {tasks}
       </div>
 
-      <button className="ml-12 mt-2 text-sm" onClick={() => addTask()}>Add task...</button>
+      <button onClick={() => addTask()} className="ml-12 mt-2 text-sm">Add task...</button>
     </div>
   )
 }
