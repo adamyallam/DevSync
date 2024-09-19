11d58673263fb36ec76055bc0a77f2a116335d6f
Adam Allam <adamyallam@gmail.com>
2024-09-19 13:26:38 -0500

    Added logic to creating a board task

--- src/components/dashboard/pages/BoardTaskSection.tsx
+++ src/components/dashboard/pages/BoardTaskSection.tsx
@@ -1,25 +1,55 @@
 'use client'
 import { Plus, Ellipsis } from "lucide-react"
+import { useState, useEffect } from "react"
 
 //component imports
 import ProjectBoardTask from "./projects/board/ProjectBoardTask"
 import AutoResizingInput from "@/components/styledElements/AutoResizingInput"
 
-export const BoardTaskSection = () => {
+interface Props {
+  hasInitialTask: boolean;
+}
+
+export const BoardTaskSection: React.FC<Props> = ({hasInitialTask}) => {
+  const [initialized, setInitialized] = useState<boolean>(false);
+  const [boardTasks, setBoardTasks] = useState<JSX.Element[]>([]);
+
+  useEffect(() => {
+    if (hasInitialTask) {
+      setBoardTasks([newBoardTask()]);
+      setInitialized(true);
+    } else {
+      setInitialized(true);
+    }
+  }, [hasInitialTask]);
+
+  function newBoardTask() {
+    return <ProjectBoardTask key={boardTasks.length + 1} />
+  };
+
+  function addBoardTask() {
+    setBoardTasks((prevTasks) => {
+      return [...prevTasks, newBoardTask()]
+    })
+  }
+
+  if (!initialized) {
+    return null
+  }
+
   return (
     <div className="flex flex-col ml-8 mt-5 border rounded-lg border-gray-400 w-[270px] h-[calc(100%-40px)]">
       <div className="flex justify-between ml-2 mb-3 mt-2 mr-2">
         <AutoResizingInput maxGrowthWidth={200} placeholder="Untitled Section"/>
 
         <div className="flex gap-2">
-          <button><Plus size={20} /></button>
-          <button><Ellipsis size={20} /></button>
+          <button onClick={() => addBoardTask()}><Plus size={18} /></button>
+          <button><Ellipsis size={18} /></button>
         </div>
       </div>
 
       <div className="self-center overflow-auto">
-        <ProjectBoardTask />
-
+        {boardTasks}
       </div>
     </div>
   )
