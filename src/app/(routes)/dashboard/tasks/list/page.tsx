244f72981b0091a7d475d7d5a41ce33abc63d24b
Adam Allam <adamyallam@gmail.com>
2024-09-19 13:39:13 -0500

    removed unused imports

--- src/app/(routes)/dashboard/tasks/list/page.tsx
+++ src/app/(routes)/dashboard/tasks/list/page.tsx
@@ -1,7 +1,6 @@
 
 // Component Imports
 import MyTasksManager from '@/components/dashboard/pages/tasks/list/MyTasksManager'
-import TaskSection from '@/components/dashboard/pages/sharedTasks/list/TaskSection'
 import CreateTaskSection from '@/components/dashboard/pages/sharedTasks/list/CreateTaskSection'
 
 export default async function list() {
