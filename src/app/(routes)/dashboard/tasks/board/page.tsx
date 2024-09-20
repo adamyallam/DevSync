0c936f86c96f13ce028b9bf220bb5c5aa18efc8a
Adam Allam <adamyallam@gmail.com>
2024-09-20 12:25:10 -0500

    added components to MyTasks board

--- src/app/(routes)/dashboard/tasks/board/page.tsx
+++ src/app/(routes)/dashboard/tasks/board/page.tsx
@@ -1,11 +1,16 @@
 
 // Component Imports
+import AddTaskButton from "@/components/styledElements/AddTaskButton"
+import CreateBoardSection from "@/components/dashboard/pages/sharedTasks/board/CreateBoardSection"
 
 export default async function board() {
 
   return (
-    <div>
-      
+    <div className="flex flex-col w-full h-full">
+      <AddTaskButton />
+      <div className="overflow-x-hidden overflow-y-auto w-full h-full bg-gray-50 border-t-2 border-gray-100">
+        <CreateBoardSection />
+      </div>
     </div>
   )
 
