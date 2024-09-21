4151319da50563cf4d4ec0e0cdf4c067db99eab8
Adam Allam <adamyallam@gmail.com>
2024-09-21 16:02:59 -0500

    Switched overflow to auto along the x axis

--- src/app/(routes)/dashboard/tasks/board/page.tsx
+++ src/app/(routes)/dashboard/tasks/board/page.tsx
@@ -8,7 +8,7 @@ export default async function board() {
   return (
     <div className="flex flex-col w-full h-full">
       <AddTaskButton />
-      <div className="overflow-x-hidden overflow-y-auto w-full h-full bg-gray-50 border-t-2 border-gray-100">
+      <div className="overflow-auto w-full h-full bg-gray-50 border-t-2 border-gray-100">
         <CreateBoardSection />
       </div>
     </div>
