aaea31747f9a7a06123b35199b96cfc443499632
Adam Allam <adamyallam@gmail.com>
2024-09-23 13:22:29 -0500

    added flex flex-col classes

--- src/app/(routes)/dashboard/tasks/calendar/page.tsx
+++ src/app/(routes)/dashboard/tasks/calendar/page.tsx
@@ -5,7 +5,7 @@ import MyCalendar from "@/components/dashboard/pages/calendar/MyCalendar"
 export default async function calendar() {
 
   return (
-    <div className="h-full w-full">
+    <div className="flex flex-col h-full w-full ">
       <MyCalendar />
     </div>
   )
