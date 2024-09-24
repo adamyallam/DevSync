e152c7726f8e996e193802181a2481cb49fb0d22
Adam Allam <adamyallam@gmail.com>
2024-09-24 14:51:06 -0500

    Added addTaskHeaderButton

--- src/components/dashboard/pages/calendar/CalendarManager.tsx
+++ src/components/dashboard/pages/calendar/CalendarManager.tsx
@@ -1,11 +1,15 @@
 'use client'
 
 //component imports
+import AddTaskHeaderButton from "@/components/styledElements/AddTaskHeaderButton"
 
 const CalendarManager = () => {
   return (
-    <div>
-
+    <div >
+      <div className="flex items-center">
+        <AddTaskHeaderButton showFilterSort={false} />
+        <div className="border-r-2 border-black h-5"/>
+      </div>
     </div>
   )
 }
