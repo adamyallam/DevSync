7ebc0e7823d83afbac443314b5ebd09c6e2c7d7c
Adam Allam <adamyallam@gmail.com>
2024-09-24 16:09:54 -0500

    Added temporary content to monthCalendar component

---
+++ src/app/(routes)/dashboard/tasks/calendar/month/page.tsx
@@ -0,0 +1,13 @@
+
+// Component Imports
+import WeekCalendar from "@/components/dashboard/pages/calendar/WeekCalendar"
+
+export default async function monthCalendar() {
+
+  return (
+    <div className="flex flex-col h-full w-full ">
+      <WeekCalendar />
+    </div>
+  )
+
+}
