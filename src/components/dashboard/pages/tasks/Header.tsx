799d510ffa637886c60b16c9e859240fe3032414
Adam Allam <adamyallam@gmail.com>
2024-09-24 16:29:02 -0500

    Adjusted layout

--- src/components/dashboard/pages/tasks/Header.tsx
+++ src/components/dashboard/pages/tasks/Header.tsx
@@ -10,21 +10,21 @@ export const Header = () => {
     return (
       <div className="mt-16">
         <div className="flex items-center mb-2"> 
-          <div className="w-12 h-12 ml-8 border-2 border-gray-600 bg-gray-200 rounded-full" />
-          <div className="ml-4">
-            <div className="flex items-center gap-1">
+          <div className="border-b-2 border-gray-300 w-full">
+            <div className="flex ml-8 items-center gap-1">
+              <div className="w-8 h-8 border-2 border-gray-600 bg-gray-200 rounded-full mr-2" />
               <h1 className="text-xl">My Tasks</h1>
               <ChevronDown size={20} strokeWidth={2} className="mt-1"/>
             </div>
-            <div className="flex gap-4">
-              <Link href='/dashboard/tasks/list' className="z-10">List<div className="bg-black w-full h-[2.5px] translate-y-[9.5px]"/></Link>
-              <Link href={'/dashboard/tasks/board'} className="z-10">Board<div className="bg-black w-full h-[2.5px] translate-y-[9.5px]"/></Link>
-              <Link href={'/dashboard/tasks/calendar/week'} className="z-10">Calendar<div className="bg-black w-full h-[2.5px] translate-y-[9.5px]"/></Link>
-              <Link href={'/dashboard/tasks/files'} className="z-10">Files<div className="bg-black w-full h-[2.5px] translate-y-[9.5px]"/></Link>
+
+            <div className="flex ml-9 gap-4 mt-1">
+              <Link href='/dashboard/tasks/list' className="z-10">List<div className="bg-black w-full h-[2.5px] translate-y-[1.5px]"/></Link>
+              <Link href={'/dashboard/tasks/board'} className="z-10">Board<div className="bg-black w-full h-[2.5px] translate-y-[1.5px]"/></Link>
+              <Link href={'/dashboard/tasks/calendar/week'} className="z-10">Calendar<div className="bg-black w-full h-[2.5px] translate-y-[1.5px]"/></Link>
+              <Link href={'/dashboard/tasks/files'} className="z-10">Files<div className="bg-black w-full h-[2.5px] translate-y-[1.5px]"/></Link>
             </div>
           </div>
         </div>
-        <div className="bg-gray-300 w-full h-[1.5px] z-0" />
       </div>
     )
 }
