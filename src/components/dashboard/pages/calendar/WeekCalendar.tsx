8c195bad6d526c57f4f10d0c49590556e879524d
Adam Allam <adamyallam@gmail.com>
2024-09-26 14:41:37 -0500

    Tasks now change in size depending on if Weekends are showing or not

--- src/components/dashboard/pages/calendar/WeekCalendar.tsx
+++ src/components/dashboard/pages/calendar/WeekCalendar.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useState, useRef, useEffect} from 'react';
 import { Plus } from 'lucide-react';
-import useWeekDates from '@/utils/hooks/useWeekDates';
+import useWeekDays from '@/utils/hooks/useWeekDays';
 import useCalendarUIContext from '@/utils/hooks/useCalendarUIContext';
 
 //Component imports
@@ -11,16 +11,34 @@ import AutoResizingInput from '@/components/styledElements/AutoResizingInput';
 const WeekCalendar = () => {
   const { isWeekendShowing, weekOrMonth } = useCalendarUIContext();
 
-  const weeks = useWeekDates()
+  const week = useWeekDays()
   const currentDate = new Date();
   const currentDay = currentDate.getDate();
 
+  const [initialWidth, setInitialWidth] = useState<number>(250)
   const [calendarTasks, setCalendarTasks] = useState<JSX.Element[][]>(
-    Array.from({ length: weeks.length }, () => [])
+    Array.from({ length: week.length }, () => [])
   );
 
+  useEffect(() => {
+    setInitialWidth(isWeekendShowing ? 200 : 250)
+  }, [isWeekendShowing])
+
+  useEffect(() => {
+    setCalendarTasks((prevTasks) =>
+      prevTasks.map((tasksForIndex, index) => {
+        if (!tasksForIndex) return [];
+        return tasksForIndex.map((_, taskIndex) => (
+          <div className='pb-2' key={taskIndex}>
+            <AutoResizingInput initialWidth={initialWidth} maxGrowthWidth={initialWidth} />
+          </div>
+        ));
+      })
+    );
+  }, [initialWidth]);
+
   const addTask = (index: number) => {
-    const calendarTask = <div className='pb-2'><AutoResizingInput initialWidth={250} maxGrowthWidth={250} key={Date.now()} /></div>
+    const calendarTask = <div key={calendarTasks[index]?.length || 0} className='pb-2'><AutoResizingInput initialWidth={initialWidth} maxGrowthWidth={initialWidth} /></div>
 
     setCalendarTasks((prevTasks) => {
       const updatedTasks = [...prevTasks];
@@ -28,7 +46,7 @@ const WeekCalendar = () => {
       if (!updatedTasks[index]) {
         updatedTasks[index] = [];
       }
-      updatedTasks[index] = [...updatedTasks[index], calendarTask];
+      updatedTasks[index].push(calendarTask)
 
       return updatedTasks;
     });
@@ -51,7 +69,7 @@ const WeekCalendar = () => {
     <div className="h-full w-full flex border-t border-l">
       {weekOrMonth === 'Weeks' ? (
 
-        weeks.map((week, index) => (
+        week.map((week, index) => (
           <div key={index} className={`border-r border-b flex flex-col ${isWeekendShowing ? `w-full` : `${index === 0 || index === 6 ? 'w-1/4' : 'w-full'}`}`}>
 
             <div className="h-[4.5rem] p-2 border-b text-start">
