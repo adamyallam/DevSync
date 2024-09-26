ddb12b0ebe06de592bb02a3267e7a1ecc048457c
Adam Allam <adamyallam@gmail.com>
2024-09-26 13:24:25 -0500

    Added addTask functionality and monthCalendar

--- src/components/dashboard/pages/calendar/WeekCalendar.tsx
+++ src/components/dashboard/pages/calendar/WeekCalendar.tsx
@@ -5,18 +5,40 @@ import useWeekDates from '@/utils/hooks/useWeekDates';
 import useCalendarUIContext from '@/utils/hooks/useCalendarUIContext';
 
 //Component imports
+import MonthCalendar from './MonthCalendar';
+import AutoResizingInput from '@/components/styledElements/AutoResizingInput';
 
 const WeekCalendar = () => {
+  const { isWeekendShowing, weekOrMonth } = useCalendarUIContext();
+
   const weeks = useWeekDates()
   const currentDate = new Date();
   const currentDay = currentDate.getDate();
 
-  const { isWeekendShowing, weekOrMonth } = useCalendarUIContext();
+  const [calendarTasks, setCalendarTasks] = useState<JSX.Element[][]>(
+    Array.from({ length: weeks.length }, () => [])
+  );
+
+  const addTask = (index: number) => {
+    const calendarTask = <div className='pb-2'><AutoResizingInput initialWidth={250} maxGrowthWidth={250} key={Date.now()} /></div>
+
+    setCalendarTasks((prevTasks) => {
+      const updatedTasks = [...prevTasks];
+      
+      if (!updatedTasks[index]) {
+        updatedTasks[index] = [];
+      }
+      updatedTasks[index] = [...updatedTasks[index], calendarTask];
+
+      return updatedTasks;
+    });
+  };
 
   const addTaskButton = (index: number) => {
+
     if(index !== 0 && index !== 6 || isWeekendShowing) {
       return (
-        <button className="flex items-center gap-1 pb-2 font-semibold opacity-60 hover:opacity-100">
+        <button onClick={() => {addTask(index)}} className="flex items-center gap-1 pb-2 font-semibold opacity-60 hover:opacity-100">
           <Plus size={18}/>
           Add Task
         </button>
@@ -38,6 +60,8 @@ const WeekCalendar = () => {
             </div>
 
             <div className="flex flex-col items-center flex-grow bg-gray-100 pt-4">
+                {calendarTasks[index]?.map((task) => task)}
+
               {addTaskButton(index)}
             </div>
           </div>
@@ -45,11 +69,7 @@ const WeekCalendar = () => {
 
         ) : (
 
-          <div>
-            <div>
-              <h1>random text</h1>
-            </div>
-          </div>
+          <MonthCalendar />
 
           )}
     </div>
