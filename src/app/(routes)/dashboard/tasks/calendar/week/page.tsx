9bd5e3dffec343f5f749192de34ecb107b3de629
Adam Allam <adamyallam@gmail.com>
2024-09-27 12:31:21 -0500

    changed weekCalendar component to Calendar component

--- src/app/(routes)/dashboard/tasks/calendar/week/page.tsx
+++ src/app/(routes)/dashboard/tasks/calendar/week/page.tsx
@@ -1,12 +1,12 @@
 
 // Component Imports
-import WeekCalendar from "@/components/dashboard/pages/calendar/WeekCalendar"
+import Calendar from "@/components/dashboard/pages/calendar/Calendar"
 
 export default async function weekCalendar() {
 
   return (
     <div className="h-full w-full">
-      <WeekCalendar />
+      <Calendar />
     </div>
   )
 
