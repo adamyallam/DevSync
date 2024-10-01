a5f3de06f244f5ab365cfd935827125e69045bbf
Adam Allam <adamyallam@gmail.com>
2024-10-01 17:29:03 -0500

    Added Month display

--- src/components/dashboard/pages/calendar/Calendar.tsx
+++ src/components/dashboard/pages/calendar/Calendar.tsx
@@ -1,7 +1,8 @@
 'use client'
 import React, { useState, useRef, useEffect} from 'react';
 import { Plus } from 'lucide-react';
-import useWeekDays from '@/utils/hooks/useWeekDays';
+import useCalendarWeek from '@/utils/hooks/useCalendarWeek';
+import useCalendarMonth from '@/utils/hooks/useCalendarMonth';
 import useCalendarUIContext from '@/utils/hooks/useCalendarUIContext';
 
 //Component imports
@@ -9,10 +10,11 @@ import AutoResizingInput from '@/components/styledElements/AutoResizingInput';
 
 const Calendar = () => {
   const { isWeekendShowing, weekOrMonth } = useCalendarUIContext();
-
-  const week = useWeekDays()
   const currentDate = new Date();
   const currentDay = currentDate.getDate();
+
+  const week = useCalendarWeek()
+  const month = useCalendarMonth()
 
   const [initialWidth, setInitialWidth] = useState<number>(250)
   const [calendarTasks, setCalendarTasks] = useState<JSX.Element[][]>(
@@ -71,12 +73,12 @@ const Calendar = () => {
   );
 
   return (
-    <div className="h-full w-full flex border-t border-l">
-      {weekOrMonth === 'Weeks' ? (
+    <div className="h-full w-full flex">
+      {weekOrMonth === 'Week' ? (
         week.map((week, index) => (
-          <div key={index} className={`border-r border-b flex flex-col h-full ${isWeekendShowing ? `w-full` : `${index === 0 || index === 6 ? 'w-1/4' : 'w-full'}`}`}>
+          <div key={index} className={`border flex flex-col h-full ${isWeekendShowing ? `w-full` : `${index === 0 || index === 6 ? 'w-1/4' : 'w-full'}`}`}>
 
-            <div className="min-h-[4.5rem] p-2 border-b text-start">
+            <div className="min-h-[4.75rem] p-2 border-b text-start">
               <div className="text-sm font-semibold text-gray-600">{week.day}</div>
               <div className={`text-lg mt-1 ${currentDay === week.date ? 'flex items-center justify-center border bg-blue-500 w-9 h-8 rounded-md text-white' : ''}`}>{week.date}</div>
             </div>
@@ -91,25 +93,31 @@ const Calendar = () => {
                   (calendarTasks[index]?.map((task) => task)) 
                 )
               }
-
               {addTaskButton(index)}
             </div>
           </div>
         ))
 
         ) : (
-          <div className='h-full w-full flex'>
-            {week.map((week, index) => (
-              <div key={index} className='flex flex-col h-full w-full'>
-                <div className='flex justify-evenly w-full border'>
-                  <span className='text-sm'>{week.day}</span>
+          <div className='flex flex-col h-full w-full'>
+            <div className={`flex w-full border`}>
+              {week.map((week, index) => (                
+                <div className={`${isWeekendShowing ? `w-full` : `${index === 0 || index === 6 ? 'w-1/5' : 'w-full'}`}`}>
+                  <div className={`p-1 pl-2 text-start font-semibold text-gray-600 text-xs w-full ${(index === 0 || index === 5 || index === 6) ? (isWeekendShowing ? 'border-r' : '') : 'border-r'}`}>{!isWeekendShowing && (index === 0 || index === 6) ? <div /> : week.day}</div>
                 </div>
-                  
-                <div className='grid grid-cols-7 grid-rows-5 border'>
-                  <span className='ml-3'>{week.date}</span>
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
+
+            <div className={`grid grid-rows-5 pt-1 h-full w-full ${isWeekendShowing ? 'grid-cols-7' : 'grid-cols-[repeat(27,minmax(0,1fr))]'}`}>
+              {month.map((day, index) => {
+                const columnIndex = index % 7;
+                const isWeekend = columnIndex === 0 || columnIndex === 6;
+
+                return (
+                  <div key={index} className={`pl-2 pt-1 border ${isWeekendShowing ? 'col-span-1' : (isWeekend ? 'col-span-1' : 'col-span-5')}`}>{day}</div>
+                );
+              })}
+            </div>
           </div>
         )
       }
