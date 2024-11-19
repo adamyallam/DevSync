5c65774b1e28c1b58b3d6aeedb1fc81c29756ed0
Adam Allam <adamyallam@gmail.com>
2024-11-19 16:25:01 -0600

    Refactored middleware to be more secure and efficient

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,13 @@
 import {NextResponse, NextRequest} from 'next/server'
-
+import { getToken } from "next-auth/jwt"; 
 
 export default async function middleware(req: NextRequest) {
-    const session = await req.cookies.get('next-auth.session-token')
+    const session = await getToken({ req });
 
     if (!session && req.nextUrl.pathname.startsWith('/dashboard')) {
-        return NextResponse.redirect(new URL('http://localhost:3000/user/registration/signin', req.url))
+        return NextResponse.redirect(new URL('/user/registration/signin', req.nextUrl.origin));
     } else if (session && req.nextUrl.pathname.startsWith('/user/registration')) {
-        return NextResponse.redirect(new URL('http://localhost:3000/dashboard/home', req.url))
+        return NextResponse.redirect(new URL('/dashboard/home', req.nextUrl.origin));
     }
 }
 
