472df4a947961ce661d9ea5b30515db7e7746c78
Adam Allam <adamyallam@gmail.com>
2024-11-22 17:13:14 -0600

    Added project id to route so each project has their own route

--- src/components/styledElements/ProjectLink.tsx
+++ src/components/styledElements/ProjectLink.tsx
@@ -4,9 +4,10 @@ import { usePathSegments } from '@/utils/hooks/usePathSegments';
 
 interface Props {
   name: React.ReactNode;
+  id: number
 }
 
-export const ProjectLink: React.FC<Props> = ({name}) => {
+export const ProjectLink: React.FC<Props> = ({name, id}) => {
 
   const currentPath = usePathSegments(2);
   function applySidebarClass(...pagePaths: string[]) {
@@ -20,7 +21,7 @@ export const ProjectLink: React.FC<Props> = ({name}) => {
 
   return (
     <>
-    <Link href='/dashboard/projects/board' className={`flex items-center h-8 ${applySidebarClass('projects/overview', 'projects/list', 'projects/board', 'projects/calendar', 'projects/files')}`}>
+    <Link href={`/dashboard/projects/${id}/list`} className={`flex items-center h-8 ${applySidebarClass('projects/overview', 'projects/list', 'projects/board', 'projects/calendar', 'projects/files')}`}>
       <div className='border-2 bg-white rounded-md w-4 h-4' />
       <span className='ml-2 text-sm'>{name}</span>
     </Link>
