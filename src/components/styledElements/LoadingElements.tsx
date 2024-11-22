5af6855c1c3ebdc3bd55b57166b40d4e40e82ecd
Adam Allam <adamyallam@gmail.com>
2024-11-22 17:11:45 -0600

    Added loading animation

---
+++ src/components/styledElements/LoadingElements.tsx
@@ -0,0 +1,9 @@
+export const BouncingDots = () => {
+  return (
+    <div className="flex items-center gap-1">
+      <div className="bounce w-[5px] h-[5px] bg-white rounded-full" />
+      <div className="bounce w-[5px] h-[5px] bg-white rounded-full" />
+      <div className="bounce w-[5px] h-[5px] bg-white rounded-full" />
+    </div>
+  );
+};
