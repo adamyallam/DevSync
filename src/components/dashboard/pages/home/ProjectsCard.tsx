d4f11516ec55c7193dda05c49e6e0021c17a4004
Adam Allam <adamyallam@gmail.com>
2024-11-23 23:37:37 -0600

    Projects in the projectsCard component are now dynamicaly rendered

--- src/components/dashboard/pages/home/ProjectsCard.tsx
+++ src/components/dashboard/pages/home/ProjectsCard.tsx
@@ -1,10 +1,14 @@
 'use client'
 import { ChevronDown, Plus } from "lucide-react"
-
+import useProjectsDataContext from "@/utils/hooks/context/useProjectDataProvider"
 
 // Component Imports
+import ProjectBoxLink from "@/components/styledElements/ProjectBoxLink"
+import { RotatingCircle } from "@/components/styledElements/LoadingElements"
 
 export const ProjectsCard = () => {
+
+  const { projects } = useProjectsDataContext()
 
   return (
     <div className="w-550 h-96 border-2 border-gray-200 rounded-lg shadow-sm">
@@ -14,28 +18,26 @@ export const ProjectsCard = () => {
         <ChevronDown size={14} strokeWidth={2} className="mt-2" />
       </div>
 
-      <div className="grid grid-cols-2 grid-rows-4 ml-10 mt-6 gap-3">
-        <div className="flex items-center">
-          <div className="flex items-center justify-center border-2 border-dotted border-gray-400 w-14 h-14 rounded-xl">
-            <Plus size={24} />
+      <div className="grid grid-cols-2 grid-rows-5 ml-10 mt-6 gap-6">
+        {projects?.length ? (
+          <>
+            <div className="flex items-center">
+              <div className="flex items-center justify-center border-2 border-dotted border-gray-400 w-10 h-10 rounded-xl">
+                <Plus size={24} />
+              </div>
+              <p className="ml-3">Create project</p>
+            </div>
+
+            {projects.map((project) => (
+              <ProjectBoxLink key={project.id} projectID={project.id} name={project.name} />
+            ))}
+          </>
+        ) : (
+          <div className="flex justify-center items-center col-span-2 row-span-10">
+            <RotatingCircle />
           </div>
-          <p className="ml-3">Create project</p>
-        </div>
+        )}
 
-        <div className="flex items-center">
-          <div className="border-2 border-red-500 bg-red-300 w-14 h-14 rounded-xl ml-5" />
-          <p className="ml-3">Project</p>
-        </div>
-
-        <div className="flex items-center">
-          <div className="border-2 border-blue-500 bg-blue-300 w-14 h-14 rounded-xl" />
-          <p className="ml-3">Project</p>
-        </div>
-
-        <div className="flex items-center">
-          <div className="border-2 border-green-500 bg-green-300 w-14 h-14 rounded-xl ml-5" />
-          <p className="ml-3">Project</p>
-        </div>
       </div>
     </div>
   )
