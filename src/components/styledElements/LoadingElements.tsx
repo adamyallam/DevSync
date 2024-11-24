e13f4915dd56a0b182d239a982bcc9b82de43d8c
Adam Allam <adamyallam@gmail.com>
2024-11-23 23:36:44 -0600

    Added rotating circle loading animation

--- src/components/styledElements/LoadingElements.tsx
+++ src/components/styledElements/LoadingElements.tsx
@@ -1,9 +1,29 @@
-export const BouncingDots = () => {
+'use client'
+
+interface Props {
+  color: string
+  size?: number
+}
+
+export const BouncingDots: React.FC<Props> = ({ color, size = 5 }) => {
+
+  const gapSize = 0.8 * size
+
   return (
-    <div className="flex items-center gap-1">
-      <div className="bounce w-[5px] h-[5px] bg-white rounded-full" />
-      <div className="bounce w-[5px] h-[5px] bg-white rounded-full" />
-      <div className="bounce w-[5px] h-[5px] bg-white rounded-full" />
+    <div style={{ gap: `${gapSize}px` }} className="flex items-center">
+      <div style={{ backgroundColor: color, width: `${size}px`, height: `${size}px` }} className="bounce rounded-full" />
+      <div style={{ backgroundColor: color, width: `${size}px`, height: `${size}px` }} className="bounce rounded-full" />
+      <div style={{ backgroundColor: color, width: `${size}px`, height: `${size}px` }} className="bounce rounded-full" />
     </div>
   );
 };
+
+
+
+export const RotatingCircle = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-16 h-16 border-4 border-t-4 border-t-gray-500 border-gray-700 border-solid rounded-full animate-spin-custom"></div>
+    </div>
+  )
+}
