1394d26b312a2bdced4fc22660d8f3e3c7d5daec
Adam Allam <adamyallam@gmail.com>
2024-11-23 22:28:46 -0600

    Formatting

--- src/components/dashboard/pages/home/UserWelcome.tsx
+++ src/components/dashboard/pages/home/UserWelcome.tsx
@@ -6,27 +6,27 @@ import { useSession } from 'next-auth/react';
 // Component Imports
 
 
-export const UserWelcome = () => { 
+export const UserWelcome = () => {
 
-  const {data: session, status} = useSession()
-    
-    return (
-      <div>
-        <div className='flex flex-col'>
-          <p className='text-center text-lg'>{getCurrentDateDisplay()}</p>
-          <h1 className='text-center mt-2 text-3xl'>Good {getDayPeriod()}, {session?.firstName}</h1>
-        </div>
-        <div className='flex justify-center'>
-          <div className='p-4 mt-4 w-[500px] bg-gray-100 rounded-full'>
-            <div className='grid grid-rows-1 grid-cols-3 justify-center divide-x divide-black'>
-                <p className='flex justify-center text-sm'>My Week</p>
-                <p className='flex justify-center text-sm'>Tasks Completed</p>
-                <p className='flex justify-center text-sm'>Collaborators</p>
-            </div>
+  const { data: session, status } = useSession()
+
+  return (
+    <div>
+      <div className='flex flex-col'>
+        <p className='text-center text-lg'>{getCurrentDateDisplay()}</p>
+        <h1 className='text-center mt-2 text-3xl'>Good {getDayPeriod()}, {session?.firstName}</h1>
+      </div>
+      <div className='flex justify-center'>
+        <div className='p-4 mt-4 w-[500px] bg-gray-100 rounded-full'>
+          <div className='grid grid-rows-1 grid-cols-3 justify-center divide-x divide-black'>
+            <p className='flex justify-center text-sm'>My Week</p>
+            <p className='flex justify-center text-sm'>Tasks Completed</p>
+            <p className='flex justify-center text-sm'>Collaborators</p>
           </div>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 
