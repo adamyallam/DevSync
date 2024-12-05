654d5e6f8e04ad310663264d28a02d7ecba65f76
Adam Allam <adamyallam@gmail.com>
2024-12-04 22:12:30 -0600

    Now creates a project on submission of form

--- src/components/dashboard/pages/projects/CreateProjectForm.tsx
+++ src/components/dashboard/pages/projects/CreateProjectForm.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
 import { X, Calendar, Search } from 'lucide-react';
 import DatePickerField from '@/components/styledElements/DatePickerField';
+import { getEndOfNextMonth } from '@/utils/dateFunctions/getDateFunctions';
+import useNavbarUIContext from '@/utils/hooks/context/useNavbarUIContext';
 
 interface Props {
   isOpen: boolean;
@@ -9,9 +11,44 @@ interface Props {
 }
 
 const CreateProjectForm: React.FC<Props> = ({ isOpen, onClose }) => {
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const { toggleCreateProjectForm, isCreateProjectFormOpen } = useNavbarUIContext();
+
+  const [projectDueDate, setProjectDueDate] = useState<Date | null>(null);
   const [projectName, setProjectName] = useState("");
-  const [description, setDescription] = useState("");
+  const [projectDescription, setProjectDescription] = useState("");
+
+  const createProject = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const projectData = {
+      name: projectName,
+      description: projectDescription,
+      dueDate: projectDueDate || getEndOfNextMonth()
+    };
+
+    try {
+      const res = await fetch("http://localhost:3000/api/project", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(projectData)
+      });
+
+      const result = await res.json();
+
+      if (res.status === 201) {
+        console.log("Project Created:", result.project);
+      } else {
+        console.log('An error occured, please try again later')
+      }
+    } catch (error) {
+      console.error("Error creating project:", error);
+    }
+
+    toggleCreateProjectForm(!isCreateProjectFormOpen)
+  };
+
 
   if (!isOpen) return null;
 
@@ -23,12 +60,12 @@ const CreateProjectForm: React.FC<Props> = ({ isOpen, onClose }) => {
           <h1 className='fixed text-2xl font-semibold'>New Project</h1>
         </div>
 
-        <form className='flex w-full h-full'>
+        <form onSubmit={createProject} className='flex w-full h-full'>
           <div className='flex flex-col gap-10 justify-center items-center w-full h-full'>
 
             <div className='flex flex-col self-start w-full ml-9'>
-              <span className='text-xs ml-1 font-semibold'>Project Name:</span>
-              <input className={`pl-1 p-4 h-7 w-4/5 border-b-4 bg-[#1b1717] outline-none ${projectName ? 'border-[#bdb6b6]' : 'border-[#262222]'} focus:border-[#bdb6b6] hover:border-[#bdb6b6]`} 
+              <span className='text-xs ml-1 font-semibold'>* Project Name:</span>
+              <input className={`pl-1 p-4 h-7 w-4/5 border-b-4 bg-[#1b1717] outline-none ${projectName ? 'border-[#bdb6b6]' : 'border-[#262222]'} focus:border-[#bdb6b6] hover:border-[#bdb6b6]`}
                 placeholder='Name'
                 value={projectName}
                 onChange={(e) => setProjectName(e.target.value)}
@@ -38,9 +75,9 @@ const CreateProjectForm: React.FC<Props> = ({ isOpen, onClose }) => {
             <div className='flex flex-col self-start ml-9 gap-2 w-full'>
               <span className='text-xs ml-1 font-semibold'>Due Date:</span>
 
-              <div className={`group flex gap-2 w-4/5 text-[#403939] border-b-4 hover:border-[#bdb6b6] hover:text-[#bdb6b6] focus-within:border-[#bdb6b6] pb-0.5 ${selectedDate ? 'border-[#bdb6b6]' : 'border-[#262222]'}`}>
-                <Calendar className={`group-focus-within:text-[#bdb6b6] ${selectedDate ? 'text-[#bdb6b6]' : ''}`} size={20} />
-                <DatePickerField datePickerStyles={`bg-[#1b1717] text-[#bdb6b6] outline-none focus:border-[#bdb6b6] ${selectedDate ? 'border-[#bdb6b6]' : 'border-[#262222]'}`} selectedDate={selectedDate} onDateChange={setSelectedDate} />
+              <div className={`group flex gap-2 w-4/5 text-[#403939] border-b-4 hover:border-[#bdb6b6] hover:text-[#bdb6b6] focus-within:border-[#bdb6b6] pb-0.5 ${projectDueDate ? 'border-[#bdb6b6]' : 'border-[#262222]'}`}>
+                <Calendar className={`group-focus-within:text-[#bdb6b6] ${projectDueDate ? 'text-[#bdb6b6]' : ''}`} size={20} />
+                <DatePickerField datePickerStyles={`bg-[#1b1717] text-[#bdb6b6] outline-none focus:border-[#bdb6b6] ${projectDueDate ? 'border-[#bdb6b6]' : 'border-[#262222]'}`} selectedDate={projectDueDate} onDateChange={setProjectDueDate} />
               </div>
             </div>
 
@@ -53,14 +90,14 @@ const CreateProjectForm: React.FC<Props> = ({ isOpen, onClose }) => {
               </div>
             </div>
 
-            <button className='text-[#f3f4f6] border-2 border-gray-300 w-4/5 h-12 hover:font-semibold hover:text-[#bdb6b6] hover:border-[#bdb6b6] hover:scale-105 transition-transform'>Submit</button>
+            <button type='submit' className='text-[#f3f4f6] border-2 border-gray-300 w-4/5 h-12 hover:font-semibold hover:text-[#bdb6b6] hover:border-[#bdb6b6] hover:scale-105 transition-transform'>Create Project</button>
           </div>
 
           <div className='flex w-full h-full justify-center items-center'>
-            <textarea className={`w-3/4 h-3/4 p-1 pl-2 border-4 resize-none outline-none text-sm bg-[#1b1717] ${description ? 'border-[#bdb6b6]' : 'border-[#262222]'} focus:border-[#bdb6b6] hover:border-[#bdb6b6]`}
-            placeholder='Description...' 
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
+            <textarea className={`w-3/4 h-3/4 p-1 pl-2 border-4 resize-none outline-none text-sm bg-[#1b1717] ${projectDescription ? 'border-[#bdb6b6]' : 'border-[#262222]'} focus:border-[#bdb6b6] hover:border-[#bdb6b6]`}
+              placeholder='* Project Description...'
+              value={projectDescription}
+              onChange={(e) => setProjectDescription(e.target.value)}
             />
           </div>
         </form>
