43b1dc9ae1c293f51f317c56898aa81d51ae2787
Adam Allam <adamyallam@gmail.com>
2024-12-10 15:22:46 -0600

    Finished all status colors and applied them

--- src/components/styledElements/StatusButton.tsx
+++ src/components/styledElements/StatusButton.tsx
@@ -9,22 +9,23 @@ interface StatusButtonProps {
 
 const StatusButton = ({ status }: StatusButtonProps) => {
   const statusConfig = {
-    NotStarted: { label: 'Not Started', color: 'border-black', bgColor: 'bg-gray-300' },
-    OnTrack: { label: 'On Track', color: 'border-green-500', bgColor: 'bg-green-200' },
-    OffTrack: { label: 'Off Track', color: 'border-red-500', bgColor: 'bg-red-200' },
-    AtRisk: { label: 'At Risk', color: 'border-yellow-500', bgColor: 'bg-yellow-200' },
-    OnHold: { label: 'On Hold', color: 'border-blue-500', bgColor: 'bg-blue-200' },
+    NotStarted: { label: 'Not Started', textColor: 'text-gray-500', bgColor: 'bg-gray-200', secondBgColor: 'bg-gray-500' },
+    OnTrack: { label: 'On Track', textColor: 'text-[#48F701]', bgColor: 'bg-[#009903]', secondBgColor: 'bg-[#48F701]' },
+    OffTrack: { label: 'Off Track', textColor: 'text-[#FF7D7A]', bgColor: 'bg-[#A32020]', secondBgColor: 'bg-[#FF7D7A]' },
+    AtRisk: { label: 'At Risk', textColor: 'text-[#FDFF01]', bgColor: 'bg-[#D4BD02]', secondBgColor: 'bg-[#FDFF01]' },
+    OnHold: { label: 'On Hold', textColor: 'text-[#02BDFF]', bgColor: 'bg-[#1A5AB5]', secondBgColor: 'bg-[#02BDFF]' },
   };
 
-  const statusKey = status.charAt(0).toUpperCase() + status.slice(1) as StatusKey; // Capitalize the first letter for consistency
-  const { label, color, bgColor } = statusConfig[statusKey] || { label: 'No Status Found', color: 'border-gray-400', bgColor: 'bg-gray-100' };
+  const statusKey = status.charAt(0).toUpperCase() + status.slice(1) as StatusKey;
+  const { label, textColor, bgColor, secondBgColor } = statusConfig[statusKey] || { label: 'No Status Found', textColor: 'text-gray-700', bgColor: 'bg-gray-100', secondBgColor: 'bg-gray-700' };
 
   return (
-    <button className={`flex items-center gap-1 ml-3 mt-1 text-sm border rounded-full h-6 p-1 px-2 ${bgColor} ${color}`}>
-      <div className={`border rounded-full ${color} w-2 h-2`} />
-      {label}
-      <ChevronDown strokeWidth={2} size={16} />
-    </button>
+    <div className='flex items-center'>
+      <button className={`group flex items-center gap-1 ml-1 text-xs rounded-md h-6 px-1 font-semibold hover:scale-105 transition-transform ${bgColor} ${textColor}`}>
+        <div className={`rounded-full ${secondBgColor} w-2 h-2`} />
+        {label}
+      </button>
+    </div>
   );
 };
 
