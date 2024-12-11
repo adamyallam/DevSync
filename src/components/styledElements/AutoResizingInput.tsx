ed05179d32f197860269b0dc2136ebbae0d64788
Adam Allam <adamyallam@gmail.com>
2024-12-10 23:04:35 -0600

    Refactored

--- src/components/styledElements/AutoResizingInput.tsx
+++ src/components/styledElements/AutoResizingInput.tsx
@@ -5,14 +5,11 @@ interface AutoResizingInputProps {
   placeholder?: string;
   initialText?: string;
   maxGrowthWidth?: number;
-  parentClassName?: string;
-  inputClassName?: string;
-  spanClassName?: string;
 }
 
 //Input field that grows in size if characters do not fit within it's "Initial Width"
 
-export const AutoResizingInput: React.FC<AutoResizingInputProps> = ({initialWidth = 125, maxGrowthWidth, placeholder, initialText, parentClassName, inputClassName, spanClassName}) => {
+export const AutoResizingInput: React.FC<AutoResizingInputProps> = ({initialWidth = 125, maxGrowthWidth, placeholder, initialText}) => {
   const [text, setText] = useState(`${initialText || ''}`)
   const inputRef = useRef<HTMLInputElement>(null);
   const spanRef = useRef<HTMLSpanElement>(null);
@@ -28,21 +25,21 @@ export const AutoResizingInput: React.FC<AutoResizingInputProps> = ({initialWidt
   }, [text]);
 
   return (
-    <div className={parentClassName ? parentClassName : ''}>
+    <>
       <input
         type="text"
         placeholder={placeholder}
         value={text}
         onChange={(e) => setText(e.target.value)}
         ref={inputRef}
-        className={`px-1 ${inputClassName ? inputClassName : 'py-1 text-md'}`}
+        className={`px-1 py-1 text-md`}
         style={{ width: `${initialWidth}px` }}>
       </input>
 
-      <span ref={spanRef} className={`absolute top-0 left-0 invisible whitespace-pre pr-2 ${spanClassName ? spanClassName : 'text-md'}`}>
+      <span ref={spanRef} className={`absolute top-0 left-0 invisible whitespace-pre pr-2 text-md`}>
         {text}
       </span>
-    </div>
+    </>
   );
 };
 
