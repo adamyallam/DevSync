adfc942da70bc54dc7d266e7af1cab28062adb91
Adam Allam <adamyallam@gmail.com>
2024-12-12 00:31:29 -0600

    Completed statusButton

--- src/components/styledElements/StatusButton.tsx
+++ src/components/styledElements/StatusButton.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import useProjectsDataContext from "@/utils/hooks/context/useProjectDataProvider";
 import { useParams } from "next/navigation";
+import { Check } from "lucide-react";
 
 type StatusKey = 'SetStatus' | 'OnTrack' | 'OffTrack' | 'AtRisk' | 'OnHold';
 
@@ -12,20 +13,23 @@ interface StatusButtonProps {
 const StatusButton = ({ status }: StatusButtonProps) => {
   const { id } = useParams()
   const { projects } = useProjectsDataContext()
+  const statusKey = status.charAt(0).toUpperCase() + status.slice(1) as StatusKey;
+
+  const [newStatusKey, setNewStatusKey] = useState(statusKey)
   const [statusChangeOpen, setStatusChangeOpen] = useState(false)
 
   const project = projects?.find((project) => project.id.toString() === id);
 
   const statusConfig = {
-    SetStatus: { label: 'Set Status', textColor: 'text-black', bgColor: '', secondBgColor: 'border-2 border-black', borderColor: 'border-t-[#009903]' },
+    SetStatus: { label: 'Set Status', textColor: 'text-black', bgColor: '', secondBgColor: 'border-2 border-black', borderColor: '' },
     OnTrack: { label: 'On Track', textColor: 'text-[#48F701]', bgColor: 'bg-[#009903]', secondBgColor: 'bg-[#48F701]', borderColor: 'border-t-[#009903]' },
-    OffTrack: { label: 'Off Track', textColor: 'text-[#FF7D7A]', bgColor: 'bg-[#A32020]', secondBgColor: 'bg-[#FF7D7A]', borderColor: 'border-t-[#009903]' },
-    AtRisk: { label: 'At Risk', textColor: 'text-[#FDFF01]', bgColor: 'bg-[#D4BD02]', secondBgColor: 'bg-[#FDFF01]', borderColor: 'border-t-[#009903]' },
-    OnHold: { label: 'On Hold', textColor: 'text-[#02BDFF]', bgColor: 'bg-[#1A5AB5]', secondBgColor: 'bg-[#02BDFF]', borderColor: 'border-t-[#009903]' },
+    OffTrack: { label: 'Off Track', textColor: 'text-[#FF7D7A]', bgColor: 'bg-[#A32020]', secondBgColor: 'bg-[#FF7D7A]', borderColor: 'border-t-[#A32020]' },
+    AtRisk: { label: 'At Risk', textColor: 'text-[#FDFF01]', bgColor: 'bg-[#D4BD02]', secondBgColor: 'bg-[#FDFF01]', borderColor: 'border-t-[#D4BD02]' },
+    OnHold: { label: 'On Hold', textColor: 'text-[#02BDFF]', bgColor: 'bg-[#1A5AB5]', secondBgColor: 'bg-[#02BDFF]', borderColor: 'border-t-[#1A5AB5]' },
+    Complete: { label: 'Complete', textColor: 'text-green-200', bgColor: 'bg-green-700', secondBgColor: '', borderColor: 'border-t-green-700' }
   };
 
-  const statusKey = status.charAt(0).toUpperCase() + status.slice(1) as StatusKey;
-  const { label, textColor, bgColor, secondBgColor, borderColor } = statusConfig[statusKey] || { label: 'No Status Found', textColor: 'text-gray-700', bgColor: 'bg-gray-100', secondBgColor: 'bg-gray-700', borderColor: '' };
+  const { label, textColor, bgColor, secondBgColor, borderColor } = statusConfig[newStatusKey] || { label: 'No Status Found', textColor: 'text-gray-700', bgColor: 'bg-gray-100', secondBgColor: 'bg-gray-700', borderColor: '' };
 
   const changeStatus = async (newStatus: string) => {
     if (!project) return;
@@ -41,6 +45,7 @@ const StatusButton = ({ status }: StatusButtonProps) => {
       });
 
       if (res.ok) {
+        setNewStatusKey(newStatus as StatusKey)
         setStatusChangeOpen(!statusChangeOpen)
         console.log('Project name updated, new name:', newStatus)
       } else {
@@ -54,7 +59,7 @@ const StatusButton = ({ status }: StatusButtonProps) => {
   return (
     <div className='flex flex-col justify-center'>
       <button onClick={() => setStatusChangeOpen(!statusChangeOpen)} className={`group flex items-center gap-1 ml-1 rounded-md h-6 px-1 font-semibold hover:scale-105 transition-transform ${label === 'Set Status' ? 'text-sm' : 'text-xs'} ${bgColor} ${textColor}`}>
-        <div className={`rounded-full ${secondBgColor} ${label === 'Set Status' ? 'w-3 h-3' : 'w-2 h-2'}`} />
+        {newStatusKey === 'Complete' as StatusKey ? (<Check size={15} strokeWidth={3}/>) : (<div className={`rounded-full ${secondBgColor} ${label === 'Set Status' ? 'w-3 h-3' : 'w-2 h-2'}`} />)}
         {label}
       </button>
 
@@ -62,25 +67,23 @@ const StatusButton = ({ status }: StatusButtonProps) => {
         <div className={`cursor-default hover:cursor-pointer z-50 absolute left-[507px] top-24 border-4 border-[#3A4042] ${borderColor} rounded-md w-[15%] h-[31.5%] bg-[#212526] overflow-auto`}>
           <span className="flex self-center ml-1.5 px-1 pt-2 text-xs text-[#89979E] font-semibold">Update status:</span>
           <div className="flex flex-col pt-1 pb-2">
-            {Object.keys(statusConfig).map((statusKey, index) => {
-              if (statusKey !== 'SetStatus') {
-                const { label, textColor, bgColor, secondBgColor } = statusConfig[statusKey as StatusKey];
-                return (
-                  <div key={index} className="hover:bg-[#2F3636] p-1.5">
-                    <button onClick={() => changeStatus(statusKey)} className={`group flex items-center gap-1 ml-1 rounded-md h-6 px-1 font-semibold transition-transform  ${label === 'Set Status' ? 'text-sm' : 'text-xs'} ${bgColor} ${textColor}`}>
-                      <div className={`rounded-full ${secondBgColor} ${label === 'Set Status' ? 'w-3 h-3' : 'w-2 h-2'}`} />
-                      {label}
-                    </button>
-                  </div>
-                );
-              }
+            {Object.keys(statusConfig).filter((statusKey) => statusKey !== 'SetStatus' && statusKey !== 'Complete').map((statusKey, index) => {
+              const { label, textColor, bgColor, secondBgColor } = statusConfig[statusKey as StatusKey];
+              return (
+                <div onClick={() => changeStatus(statusKey)} key={index} className="hover:bg-[#2F3636] p-1.5">
+                  <button className={`group flex items-center gap-1 ml-1 rounded-md h-6 px-1 font-semibold transition-transform text-xs ${bgColor} ${textColor}`}>
+                    <div className={`rounded-full ${secondBgColor} ${label === 'Set Status' ? 'w-3 h-3' : 'w-2 h-2'}`} />
+                    {label}
+                  </button>
+                </div>
+              );
             })}
           </div>
 
           <div className="hover:bg-[#2F3636] pt-2 p-1.5 border-t-2 border-b-2 border-[#3A4042]">
-            <button onClick={() => changeStatus(statusKey)} className={`group flex items-center gap-1 ml-1 rounded-md h-6 px-1 font-semibold transition-transform  ${label === 'Set Status' ? 'text-sm' : 'text-xs'} ${bgColor} ${textColor}`}>
-              <div className={`rounded-full ${secondBgColor} ${label === 'Set Status' ? 'w-3 h-3' : 'w-2 h-2'}`} />
-              {label}
+            <button onClick={() => changeStatus('Complete')} className={`group flex items-center gap-1 ml-1 rounded-md h-6 px-1 font-semibold transition-transform text-xs ${statusConfig['Complete'].bgColor} ${statusConfig['Complete'].textColor}`}>
+              <Check size={15} strokeWidth={3}/>
+              {statusConfig['Complete'].label}
             </button>
           </div>
         </div>
