dcc91d45dbe6233276e0f64ae64ecb4f1646b588
Adam Allam <adamyallam@gmail.com>
2024-12-19 16:27:04 -0600

    Project icons are now dynamically rendered based on the projects status

--- src/components/styledElements/ProjectBoxLink.tsx
+++ src/components/styledElements/ProjectBoxLink.tsx
@@ -1,19 +1,24 @@
 'use client'
 import Link from 'next/link'
+import {statusConfig, StatusKey} from '@/utils/statusConfig'
 
 interface Props {
   name: React.ReactNode;
   projectID: number
+  status: string
 }
 
-export const ProjectBoxLink: React.FC<Props> = ({ name, projectID }) => {
+export const ProjectBoxLink: React.FC<Props> = ({ name, projectID, status }) => {
+
+  const statusKey = status.charAt(0).toUpperCase() + status.slice(1) as StatusKey;
+  const { bgColor, icon } = statusConfig[statusKey] || { bgColor: 'bg-gray-100' };
 
   return (
     <>
-      <Link href={`/dashboard/projects/${projectID}/list`} className={'flex items-center h-8'}>
-        <div className="flex items-center hover:font-bold">
-          <div className={`border-2 border-undertone bg-primary w-10 h-10 rounded-xl`} />
-          <p className="ml-3 text-sm text-primary-text ">{name}</p>
+      <Link href={`/dashboard/projects/${projectID}/list`} className={'flex items-center hover:bg-highlighted p-2 rounded-md'}>
+        <div className="flex items-center font-semibold">
+          <div className={`flex items-center justify-center border-2 border-primary ${bgColor} w-10 h-10 rounded-lg`}>{icon}</div>
+          <span className="ml-2 text-sm text-primary-text ">{name}</span>
         </div>
       </Link>
     </>
