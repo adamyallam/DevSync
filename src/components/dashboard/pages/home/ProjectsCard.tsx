74669f11a481905c6a6a7d04596091194a340bb7
Adam Allam <adamyallam@gmail.com>
2024-12-19 16:26:41 -0600

    Adjusted styling

--- src/components/dashboard/pages/home/ProjectsCard.tsx
+++ src/components/dashboard/pages/home/ProjectsCard.tsx
@@ -11,24 +11,24 @@ export const ProjectsCard = () => {
   const { projects } = useProjectsDataContext()
 
   return (
-    <div className="w-550 h-96 border-2 border-undertone rounded-lg shadow-sm">
-      <div className="flex items-center mt-6 ml-10">
+    <div className="w-550 h-96 border-2 border-undertone rounded-lg shadow-sm overflow-auto">
+      <div className="flex items-center mt-6 ml-8">
         <h1 className="text-2xl text-primary-text">Projects</h1>
         <button className="flex items-center gap-0.5 text-xs ml-2 text-secondary-text hover:font-semibold hover:scale-105 transition-transform">Recents<ChevronDown size={14} strokeWidth={3}  className="mt-1"/></button>
       </div>
 
-      <div className="grid grid-cols-2 grid-rows-5 ml-10 mt-6 gap-6">
+      <div className="grid grid-cols-2 gap-y-6 ml-8 w-[90%] h-[40%] mt-8 pb-5">
         {projects?.length ? (
           <>
-            <button className="flex items-center">
-              <div className="flex items-center justify-center border-2 border-dotted border-secondary-text w-10 h-10 rounded-xl text-primary-text">
+            <button className="flex items-center hover:bg-highlighted p-2 rounded-md">
+              <div className="flex items-center justify-center border-2 border-dotted border-secondary-text w-10 h-10 rounded-lg text-secondary-text">
                 <Plus size={20} strokeWidth={3} />
               </div>
-              <span className="ml-3 text-primary-text text-sm">Create project</span>
+              <span className="ml-3 text-secondary-text text-sm font-semibold">Create project</span>
             </button>
 
             {projects.map((project) => (
-              <ProjectBoxLink key={project.id} projectID={project.id} name={project.name} />
+              <ProjectBoxLink key={project.id} projectID={project.id} name={project.name} status={project.status}/>
             ))}
           </>
         ) : (
