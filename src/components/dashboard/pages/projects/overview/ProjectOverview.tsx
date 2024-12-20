64bf3b8f0c06251053a3dd8faa4e61f4aae122a5
Adam Allam <adamyallam@gmail.com>
2024-12-20 16:44:40 -0600

    Formatting

--- src/components/dashboard/pages/projects/overview/ProjectOverview.tsx
+++ src/components/dashboard/pages/projects/overview/ProjectOverview.tsx
@@ -15,44 +15,44 @@ export const ProjectOverview = () => {
   const { isSidebarOpen } = useNavbarUIContext()
 
   const project = projects?.find((project) => project.id.toString() === id);
-  
+
   const [value, setValue] = useState(project?.description || '');
   const [isExpanded, setIsExpanded] = useState(false);
   const [showSeeLess, setShowSeeLess] = useState(false);
-  
+
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
   useEffect(() => {
     setIsExpanded(false)
   }, [isSidebarOpen])
 
-    useEffect(() => {
-      setValue(`${project?.description || ''}`);
-    }, [project?.description]);
+  useEffect(() => {
+    setValue(`${project?.description || ''}`);
+  }, [project?.description]);
 
-    const updateProjectProperty = async (property: 'description' | 'descriptionTitle', newValue: string) => {
-      if (!project || newValue === project[property]) return;
-    
-      try {
-        const res = await fetch(`http://localhost:3000/api/project`, {
-          method: 'PATCH',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            id: project.id,
-            [property]: newValue,
-          }),
-        });
-    
-        if (res.ok) {
-          updateProject(project.id, { [property]: newValue });
-          console.log(`Project ${property} updated, new ${property}:`, newValue);
-        } else {
-          console.error(`Failed to update project ${property}`);
-        }
-      } catch (err) {
-        console.error(`Error updating project ${property}:`, err);
+  const updateProjectProperty = async (property: 'description' | 'descriptionTitle', newValue: string) => {
+    if (!project || newValue === project[property]) return;
+
+    try {
+      const res = await fetch(`http://localhost:3000/api/project`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          id: project.id,
+          [property]: newValue,
+        }),
+      });
+
+      if (res.ok) {
+        updateProject(project.id, { [property]: newValue });
+        console.log(`Project ${property} updated, new ${property}:`, newValue);
+      } else {
+        console.error(`Failed to update project ${property}`);
       }
-    };
+    } catch (err) {
+      console.error(`Error updating project ${property}:`, err);
+    }
+  };
 
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setValue(event.target.value);
@@ -62,7 +62,7 @@ export const ProjectOverview = () => {
       textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
     }
   };
-  
+
   return (
     <div className="relative flex flex-col ml-20 mt-8 h-full gap-2">
       <AutoResizingInput
@@ -86,14 +86,14 @@ export const ProjectOverview = () => {
           placeholder="Type your description, welcome message, or project info here!"
           style={{ minHeight: "150px" }}
         />
-          {!isExpanded && showSeeLess &&
-            <div className="absolute bottom-4 w-[80.5%] h-[45px] pointer-events-none bg-gradient-to-t from-secondary via-secondary/75 via-100% to-transparent" />
-          }
+        {!isExpanded && showSeeLess &&
+          <div className="absolute bottom-4 w-[80.5%] h-[45px] pointer-events-none bg-gradient-to-t from-secondary via-secondary/75 via-100% to-transparent" />
+        }
         {showSeeLess &&
-            <button onClick={() => setIsExpanded(isExpanded ? false : true)} className="self-start font-semibold text-sm text-[#02BDFF] opacity-70 hover:scale-110 hover:opacity-100 transition-transform ml-0.5">
-              {isExpanded ? 'See Less' : 'See More'}
-            </button>
-          }
+          <button onClick={() => setIsExpanded(isExpanded ? false : true)} className="self-start font-semibold text-sm text-[#02BDFF] opacity-70 hover:scale-110 hover:opacity-100 transition-transform ml-0.5">
+            {isExpanded ? 'See Less' : 'See More'}
+          </button>
+        }
       </div>
 
       <div className="h-full pb-10">
