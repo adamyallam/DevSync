0853cd2c2ea18bc51f9e097da5abdf9059229156
Adam Allam <adamyallam@gmail.com>
2024-12-20 16:37:12 -0600

    added descriptionTitle field

---
+++ src/db/prisma/migrations/20241220212928_added_description_title/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `project` ADD COLUMN `descriptionTitle` VARCHAR(60) NOT NULL DEFAULT 'Project description';
---
+++ src/db/prisma/migrations/20241220220408_changed_project_description_character_limit/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `project` MODIFY `description` VARCHAR(3000) NOT NULL;
