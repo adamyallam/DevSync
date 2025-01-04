d86dad80e2565152483e261c78602ec7e28308fd
Adam Allam <adamyallam@gmail.com>
2025-01-03 22:39:15 -0600

    Added code to create section and render it to the DOM

--- src/components/dashboard/pages/sharedComponents/list/CreateTaskSection.tsx
+++ src/components/dashboard/pages/sharedComponents/list/CreateTaskSection.tsx
@@ -1,33 +1,18 @@
 'use client'
 import { Plus } from "lucide-react"
-import { useState } from "react"
 import TaskSection from "./TaskSection"
 import { useParams } from "next/navigation"
 import useProjectsDataContext from "@/utils/hooks/context/useProjectDataProvider"
 
 export const CreateTaskSection = () => {
-  const { projects } = useProjectsDataContext()
+  const { projects, updateProjectState } = useProjectsDataContext()
   const { id } = useParams<{ id: string }>()
-
-  // const [sections, setSections] = useState<JSX.Element[]>([])
 
   const project = projects?.find((project) => project.id.toString() === id);
 
   if (!project || !project.sections) {
     return <div className='mt-16 ml-8 text-2xl'>No sections found</div>;
   }
-
-  // function addSection() {
-  //   const newSection = <TaskSection key={sections.length} isFirstSection={sections.length === 0} sectionTitle=""/>
-
-  //   setSections((prevSections) => {
-  //     return [...prevSections, newSection]
-  //   })
-  // }
-
-  // if (sections.length === 0) {
-  //   addSection()
-  // }
 
   const createSection = async () => {
 
@@ -37,13 +22,13 @@ export const CreateTaskSection = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ projectId: project.id })
+        body: JSON.stringify({ projectID: project.id })
       });
 
       const result = await res.json();
 
       if (res.status === 201) {
-        // addSection()
+        updateProjectState(project.id, { sections: [...project.sections, result.section] });
         console.log("Section Created:", result.section);
       } else {
         console.log('An error occured, please try again later')
@@ -58,7 +43,7 @@ export const CreateTaskSection = () => {
     <div className="w-full">
       <div className="w-full">
         {project.sections.map((section) => (
-          <TaskSection key={section.id} isFirstSection={project.sections.length === 0} sectionTitle={section.name || 'Untitled Sections'}/>
+          <TaskSection key={section.id} isFirstSection={project.sections.length === 0} sectionTitle={section.name || ''} />
         ))}
       </div>
 
