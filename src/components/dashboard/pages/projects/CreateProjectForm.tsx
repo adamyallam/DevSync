fa3b6dd77b9cd656d8f6cc788d0841e77d1ebf8b
Adam Allam <adamyallam@gmail.com>
2025-01-06 23:44:39 -0600

    Refactored to remove props and changed how state is managed to improve readability and fix minor errors

--- src/components/dashboard/pages/projects/CreateProjectForm.tsx
+++ src/components/dashboard/pages/projects/CreateProjectForm.tsx
@@ -6,30 +6,35 @@ import { getEndOfNextMonth } from '@/utils/dateFunctions/getDateFunctions';
 import useNavbarUIContext from '@/utils/hooks/context/useNavbarUIContext';
 import useProjectsDataContext from '@/utils/hooks/context/useProjectDataProvider';
 
-interface Props {
-  isOpen: boolean;
-  onClose: () => void;
-}
+interface ProjectData { name: string; description: string; dueDate: Date | null; defaultView: string }
 
-interface Member {
-  id: number
-}
-
-const CreateProjectForm: React.FC<Props> = ({ isOpen, onClose }) => {
+const CreateProjectForm = () => {
   const { toggleCreateProjectForm, isCreateProjectFormOpen } = useNavbarUIContext();
   const { addProject } = useProjectsDataContext()
 
-  const [projectDueDate, setProjectDueDate] = useState<Date | null>(null);
-  const [projectName, setProjectName] = useState("");
-  const [projectDescription, setProjectDescription] = useState("");
-  const [members, setMembers] = useState<Member[]>([]);
-  const [defaultView, setDefaultView] = useState('list')
+  const initialState: ProjectData = {
+    name: '',
+    description: '',
+    dueDate: getEndOfNextMonth(),
+    defaultView: 'list',
+  }
+
+  const [projectData, setProjectData] = useState<ProjectData>(initialState)
+
+  const updateProjectData = (property: keyof ProjectData, newValue: string | Date | null) => {
+    setProjectData((prevData) => ({
+      ...prevData,
+      [property]: newValue,
+    }));
+  }
+
+  const handleClose = () => {
+    toggleCreateProjectForm(!isCreateProjectFormOpen)
+    setProjectData(initialState)
+  }
+
   const [isNameValid, setIsNameValid] = useState(false);
   const [isDescriptionValid, setIsDescriptionValid] = useState(false);
-
-  const changeDefaultView = (view: string) => {
-    setDefaultView(view);
-  };
 
   const createProject = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -37,17 +42,9 @@ const CreateProjectForm: React.FC<Props> = ({ isOpen, onClose }) => {
     setIsNameValid(true);
     setIsDescriptionValid(true);
 
-    if (!projectName || !projectDescription) {
+    if (!projectData.name || !projectData.description) {
       return null;
     }
-
-    const projectData = {
-      name: projectName,
-      description: projectDescription,
-      dueDate: projectDueDate || getEndOfNextMonth(),
-      defaultView: defaultView,
-      members: members
-    };
 
     try {
       const res = await fetch("http://localhost:3000/api/project", {
@@ -62,6 +59,8 @@ const CreateProjectForm: React.FC<Props> = ({ isOpen, onClose }) => {
 
       if (res.status === 201) {
         addProject(result.project)
+        setIsNameValid(false);
+        setIsDescriptionValid(false);
         console.log("Project Created:", result.project);
       } else {
         console.log('An error occured, please try again later')
@@ -71,14 +70,15 @@ const CreateProjectForm: React.FC<Props> = ({ isOpen, onClose }) => {
     }
 
     toggleCreateProjectForm(!isCreateProjectFormOpen)
+    setProjectData(initialState)
   };
 
-  if (!isOpen) return null;
+  if (!isCreateProjectFormOpen) return null;
 
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-40 z-50 text-primary-text">
       <div className="bg-primary w-3/6 h-[78%] rounded-lg shadow-lg pt-6 relative border-[3px] border-highlighted drop-shadow-2xl">
-        <button className='fixed text-primary-text hover:text-secondary-text hover:scale-110 transition-transform pl-7'><X onClick={onClose} /></button>
+        <button className='fixed text-primary-text hover:text-secondary-text hover:scale-110 transition-transform pl-7'><X onClick={handleClose} /></button>
         <div className='flex justify-center'>
           <h1 className='fixed text-2xl font-semibold'>New Project</h1>
         </div>
@@ -90,28 +90,19 @@ const CreateProjectForm: React.FC<Props> = ({ isOpen, onClose }) => {
               <div className='flex flex-col self-start w-[93%] ml-9 h-[20%]'>
                 <span className='text-xs font-semibold'>* Project Name:</span>
 
-                <input className={`pl-1 p-4 h-[10%] w-full border-b-4 bg-primary outline-none ${isNameValid && !projectName ? "border-red-500" : projectName ? "border-primary-text" : "border-undertone"} focus:border-primary-text hover:border-primary-text`}
+                <input className={`pl-1 p-4 h-[10%] w-full border-b-4 bg-primary outline-none ${isNameValid && !projectData.name ? "border-red-500" : projectData.name ? "border-primary-text" : "border-undertone"} focus:border-primary-text hover:border-primary-text`}
                   placeholder='Name'
-                  value={projectName}
-                  onChange={(e) => setProjectName(e.target.value)}
+                  value={projectData.name}
+                  onChange={(e) => updateProjectData('name', e.target.value)}
                 />
               </div>
 
               <div className='flex flex-col self-start ml-9 gap-2 w-[93%] h-[20%]'>
                 <span className='text-xs font-semibold'>Due Date:</span>
 
-                <div className={`group flex gap-2 w-full h-[40%] text-undertone border-b-4 hover:border-primary-text hover:text-secondary-text focus-within:border-primary-text pb-0.5 ${projectDueDate ? 'border-primary-text' : 'border-undertone'}`}>
-                  <Calendar className={`group-focus-within:text-primary-text group-hover:text-primary-text flex self-center mb-1 ${projectDueDate ? 'text-primary-text' : ''}`} size={20} />
-                  <DatePickerField datePickerStyles={`w-full bg-primary text-primary-text outline-none focus:border-primary-text ${projectDueDate ? 'border-primary-text' : 'border-undertone'}`} selectedDate={projectDueDate} onDateChange={setProjectDueDate} />
-                </div>
-              </div>
-
-              <div className='flex flex-col self-start ml-9 gap-2 w-[93%] h-[20%]'>
-                <span className='text-xs font-semibold'>Add members:</span>
-
-                <div className='group flex gap-2 w-full h-[40%] text-undertone border-undertone border-b-4 focus-within:border-primary-text hover:text-secondary-text hover:border-primary-text pb-0.5'>
-                  <Search size={20} className='group-focus-within:text-secondary-text flex self-center' />
-                  <input className='w-full bg-primary focus:border-primary-text outline-none' placeholder='Search' />
+                <div className={`group flex gap-2 w-full h-[40%] text-undertone border-b-4 hover:border-primary-text hover:text-secondary-text focus-within:border-primary-text pb-0.5 ${projectData.dueDate ? 'border-primary-text' : 'border-undertone'}`}>
+                  <Calendar className={`group-focus-within:text-primary-text group-hover:text-primary-text flex self-center mb-1 ${projectData.dueDate ? 'text-primary-text' : ''}`} size={20} />
+                  <DatePickerField datePickerStyles={`w-full bg-primary text-primary-text outline-none focus:border-primary-text ${projectData.dueDate ? 'border-primary-text' : 'border-undertone'}`} selectedDate={projectData.dueDate} onDateChange={(date) => updateProjectData('dueDate', date)} />
                 </div>
               </div>
 
@@ -119,10 +110,10 @@ const CreateProjectForm: React.FC<Props> = ({ isOpen, onClose }) => {
                 <span className='text-xs ml-1 font-semibold'>Default View:</span>
 
                 <div className='flex gap-3 w-full h-full'>
-                  <button type='button' onClick={() => changeDefaultView('list')} className={`border-2 h-[100%] w-[21%] rounded-md text-xs ${defaultView === 'list' ? 'border-primary-text font-bold scale-105 bg-selected' : 'bg-highlighted border-undertone hover:border-primary-text hover:scale-105 transition-transform'}`}>List</button>
-                  <button type='button' onClick={() => changeDefaultView('overview')} className={`border-2 h-[100%] w-[21%] rounded-md text-xs ${defaultView === 'overview' ? 'border-primary-text font-bold scale-105 bg-selected' : 'bg-highlighted border-undertone hover:border-primary-text hover:scale-105 transition-transform'}`}>Overview</button>
-                  <button type='button' onClick={() => changeDefaultView('board')} className={`border-2 h-[100%] w-[21%] rounded-md text-xs ${defaultView === 'board' ? 'border-primary-text font-bold scale-105 bg-selected' : 'bg-highlighted border-undertone hover:border-primary-text hover:scale-105 transition-transform'}`}>Board</button>
-                  <button type='button' onClick={() => changeDefaultView('calendar')} className={`border-2 h-[100%] w-[21%] rounded-md text-xs ${defaultView === 'calendar' ? 'border-primary-text font-bold scale-105 bg-selected' : 'bg-highlighted border-undertone hover:border-primary-text hover:scale-105 transition-transform'}`}>Calendar</button>
+                  <button type='button' onClick={() => updateProjectData('defaultView', 'list')} className={`border-2 h-[100%] w-[21%] rounded-md text-xs ${projectData.defaultView === 'list' ? 'border-primary-text font-bold scale-105 bg-selected' : 'bg-highlighted border-undertone hover:border-primary-text hover:scale-105 transition-transform'}`}>List</button>
+                  <button type='button' onClick={() => updateProjectData('defaultView', 'overview')} className={`border-2 h-[100%] w-[21%] rounded-md text-xs ${projectData.defaultView === 'overview' ? 'border-primary-text font-bold scale-105 bg-selected' : 'bg-highlighted border-undertone hover:border-primary-text hover:scale-105 transition-transform'}`}>Overview</button>
+                  <button type='button' onClick={() => updateProjectData('defaultView', 'board')} className={`border-2 h-[100%] w-[21%] rounded-md text-xs ${projectData.defaultView === 'board' ? 'border-primary-text font-bold scale-105 bg-selected' : 'bg-highlighted border-undertone hover:border-primary-text hover:scale-105 transition-transform'}`}>Board</button>
+                  <button type='button' onClick={() => updateProjectData('defaultView', 'calendar')} className={`border-2 h-[100%] w-[21%] rounded-md text-xs ${projectData.defaultView === 'calendar' ? 'border-primary-text font-bold scale-105 bg-selected' : 'bg-highlighted border-undertone hover:border-primary-text hover:scale-105 transition-transform'}`}>Calendar</button>
                 </div>
               </div>
             </div>
@@ -130,10 +121,10 @@ const CreateProjectForm: React.FC<Props> = ({ isOpen, onClose }) => {
             <div className='flex flex-col gap-1 w-full h-full justify-center items-center'>
               <span className='text-xs ml-12 font-semibold self-start'>* Description:</span>
 
-              <textarea className={`w-[75%] h-[80%] p-1 pl-2 border-4 resize-none outline-none text-sm bg-primary ${isDescriptionValid && !projectDescription ? "border-red-500" : projectDescription ? "border-primary-text" : "border-undertone"} focus:border-primary-text hover:border-primary-text`}
+              <textarea className={`w-[75%] h-[80%] p-1 pl-2 border-4 resize-none outline-none text-sm bg-primary ${isDescriptionValid && !projectData.description ? "border-red-500" : projectData.description ? "border-primary-text" : "border-undertone"} focus:border-primary-text hover:border-primary-text`}
                 placeholder='* Project Description...'
-                value={projectDescription}
-                onChange={(e) => setProjectDescription(e.target.value)}
+                value={projectData.description}
+                onChange={(e) => updateProjectData('description', e.target.value)}
               />
             </div>
           </div>
