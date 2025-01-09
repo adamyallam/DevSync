753134811c6531e9167014d90da7a3daf4124c63
Adam Allam <adamyallam@gmail.com>
2025-01-08 23:44:27 -0600

    refactored to fit new needs

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -1,109 +1,114 @@
 import { NextRequest, NextResponse } from "next/server"
-import { createTask, deleteTask, updateTask, readTask, readAllTasks} from "src/db/db-connections/task"
+import { createTask, deleteTask, updateTask, readTask, readAllTasks } from "src/db/db-connections/task"
 import authOptions from "../auth/[...nextauth]/authOptions"
 import { getServerSession } from 'next-auth'
 
 
 //API to add or "POST" a task (invokes "createtask")
 export const POST = async (req: Request) => {
-    const { projectID, sectionID, name, description, comments, dueDate} = await req.json();
-    const session = await getServerSession(authOptions)
-    
-    try {
-        if (session) {
-            const taskData = { projectID, sectionID, name, description, comments, dueDate}
-            createTask(taskData)
-            return NextResponse.json(
-                { message: 'New task created!' },
-                { status: 201 }
-            )
-        }
-    } catch (err) {
-        return NextResponse.json(
-            { message: 'Failed to POST', err}, 
-            { status: 500 }
-        )
+  const { projectID, sectionID } = await req.json();
+  const session = await getServerSession(authOptions)
+
+  try {
+    if (session) {
+      const task = await createTask(projectID, sectionID)
+
+      return NextResponse.json(
+        { message: 'New task created!', task: task },
+        { status: 201 }
+      )
+    } else {
+      return NextResponse.json(
+        { message: 'Unauthorized' },
+        { status: 401 }
+      );
     }
+  } catch (err) {
+    return NextResponse.json(
+      { message: 'Failed to POST', err },
+      { status: 500 }
+    )
+  }
 }
 
 //API to DELETE a task (invokes "deletetask")
-export const DELETE = async (req: Request) => { 
-    const { id } = await req.json();
-    const session = await getServerSession(authOptions)
-    
-    try {
-        if (session) {
-            const taskId = { id }
-            deleteTask(taskId)
-            return NextResponse.json(
-                { message: `task ${id} Deleted!` },
-                { status: 201 }
-            )
-        }
-    } catch (err) {
-        return NextResponse.json(
-            { message: 'Failed to DELETE', err}, 
-            { status: 500 }
-        )
+export const DELETE = async (req: Request) => {
+  const { id } = await req.json();
+  const session = await getServerSession(authOptions)
+
+  try {
+    if (session) {
+      const taskId = { id }
+      deleteTask(taskId)
+      return NextResponse.json(
+        { message: `task ${id} Deleted!` },
+        { status: 201 }
+      )
     }
+  } catch (err) {
+    return NextResponse.json(
+      { message: 'Failed to DELETE', err },
+      { status: 500 }
+    )
+  }
 }
 
 //API to UPDATE a task (invokes "updatetask")
-export const PATCH = async (req: Request) => { 
-    const { id, name, description} = await req.json();
-    const session = await getServerSession(authOptions)
-    
-    try {
-        if (session) {
-            const taskId = { id }
-            const updatedInfo = {  name, description }
-            updateTask(taskId, updatedInfo)
-            return NextResponse.json(
-                { message: `task ${id} updated!` },
-                { status: 201 }
-            )
-        }
-    } catch (err) {
-        return NextResponse.json(
-            { message: 'Failed to UPDATE', err}, 
-            { status: 500 }
-        )
+export const PATCH = async (req: Request) => {
+  const { id, name, description } = await req.json();
+  const session = await getServerSession(authOptions)
+
+  try {
+    if (session) {
+      const taskId = { id }
+      const updatedInfo = { name, description }
+      updateTask(taskId, updatedInfo)
+      return NextResponse.json(
+        { message: `task ${id} updated!` },
+        { status: 201 }
+      )
     }
+  } catch (err) {
+    return NextResponse.json(
+      { message: 'Failed to UPDATE', err },
+      { status: 500 }
+    )
+  }
 }
 
 //API to READ a task (invokes "readtask")
 export const GET = async (req: NextRequest) => {
-    const searchParams = req.nextUrl.searchParams
-    const id = parseInt(searchParams.get('id') ?? '-1')
-    const session = await getServerSession(authOptions)
+  const searchParams = req.nextUrl.searchParams
+  const id = parseInt(searchParams.get('id') ?? '-1')
+  const session = await getServerSession(authOptions)
 
-    if (session) {
-        if(id !== -1){    
-            try {
-                readTask(id)
-                return NextResponse.json(
-                    { message: `task READ!` },
-                    { status: 201 }
-                )
-                } catch (err) {
-                return NextResponse.json(
-                    { message: 'Failed to READ', err}, 
-                    { status: 500 }
-                )
-            }
-        } else {
-            try {
-                readAllTasks()
-                return NextResponse.json(
-                    { message: `all users READ!` },
-                    { status: 201 }
-                )
-                } catch (err) {
-                return NextResponse.json(
-                    { message: 'Failed to READ', err}, 
-                    { status: 500 }
-                )
-            }
-        }
+  if (session) {
+    if (id !== -1) {
+      try {
+        readTask(id)
+        return NextResponse.json(
+          { message: `task READ!` },
+          { status: 201 }
+        )
+      } catch (err) {
+        return NextResponse.json(
+          { message: 'Failed to READ', err },
+          { status: 500 }
+        )
+      }
+    } else {
+      try {
+        readAllTasks()
+        return NextResponse.json(
+          { message: `all users READ!` },
+          { status: 201 }
+        )
+      } catch (err) {
+        return NextResponse.json(
+          { message: 'Failed to READ', err },
+          { status: 500 }
+        )
+      }
     }
+  }
 }
