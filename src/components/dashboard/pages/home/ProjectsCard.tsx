2d24090835f5b6493f614765241a5e439e7e9379
Adam Allam <adamyallam@gmail.com>
2025-01-08 23:46:42 -0600

    Projects card now displays createProject button if projects state is empty

--- src/components/dashboard/pages/home/ProjectsCard.tsx
+++ src/components/dashboard/pages/home/ProjectsCard.tsx
@@ -1,26 +1,37 @@
 'use client'
 import { ChevronDown, Plus } from "lucide-react"
 import useProjectsDataContext from "@/utils/hooks/context/useProjectDataProvider"
-
-// Component Imports
 import ProjectBoxLink from "@/components/styledElements/ProjectBoxLink"
 import { BouncingDots } from "@/components/styledElements/LoadingElements"
+import { StatusKey } from "@/utils/statusConfig"
+import useNavbarUIContext from "@/utils/hooks/context/useNavbarUIContext"
 
 export const ProjectsCard = () => {
-
-  const { projects } = useProjectsDataContext()
+  const { projects, loading } = useProjectsDataContext()
+  const { isCreateProjectFormOpen, toggleCreateProjectForm } = useNavbarUIContext();
 
   return (
     <div className="w-550 h-96 border-2 border-undertone rounded-lg shadow-sm overflow-auto">
       <div className="flex items-center mt-6 ml-8">
         <h1 className="text-2xl text-primary-text">Projects</h1>
-        <button className="flex items-center gap-0.5 text-xs ml-2 text-secondary-text hover:font-semibold hover:scale-105 transition-transform">Recents<ChevronDown size={14} strokeWidth={3}  className="mt-1"/></button>
+        <button className="flex items-center gap-0.5 text-xs ml-2 text-secondary-text hover:font-semibold hover:scale-105 transition-transform">Recents<ChevronDown size={14} strokeWidth={3} className="mt-1" /></button>
       </div>
 
       <div className="grid grid-cols-2 gap-y-6 ml-8 w-[90%] mt-8 pb-5">
-        {projects?.length ? (
+        {loading ? (
+          <div className="flex justify-center items-center col-span-2 row-span-10">
+            <BouncingDots color="#DBDADA" size={15} />
+          </div>
+        ) : !projects?.length ? (
+          <button onClick={() => toggleCreateProjectForm(!isCreateProjectFormOpen)} className="flex items-center hover:bg-highlighted p-2 rounded-md">
+            <div className="flex items-center justify-center border-2 border-dotted border-secondary-text w-10 h-10 rounded-lg text-secondary-text">
+              <Plus size={20} strokeWidth={3} />
+            </div>
+            <span className="ml-3 text-secondary-text text-sm font-semibold">Create project</span>
+          </button>
+        ) : (
           <>
-            <button className="flex items-center hover:bg-highlighted p-2 rounded-md">
+            <button onClick={() => toggleCreateProjectForm(!isCreateProjectFormOpen)} className="flex items-center hover:bg-highlighted p-2 rounded-md">
               <div className="flex items-center justify-center border-2 border-dotted border-secondary-text w-10 h-10 rounded-lg text-secondary-text">
                 <Plus size={20} strokeWidth={3} />
               </div>
@@ -28,15 +39,10 @@ export const ProjectsCard = () => {
             </button>
 
             {projects.map((project) => (
-              <ProjectBoxLink key={project.id} projectID={project.id} name={project.name} status={project.status}/>
+              <ProjectBoxLink key={String(project.id)} projectID={project.id} name={project.name} status={project.status ? project.status : "SetStatus" as StatusKey} />
             ))}
           </>
-        ) : (
-          <div className="flex justify-center items-center col-span-2 row-span-10">
-            <BouncingDots color="#DBDADA" size={15} />
-          </div>
         )}
-
       </div>
     </div>
   )
