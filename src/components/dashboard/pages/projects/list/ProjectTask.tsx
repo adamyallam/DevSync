48602bb22aa91a8f60b912ee2d92a0e2aa6d1395
Adam Allam <adamyallam@gmail.com>
2025-01-10 21:43:56 -0600

    added StatusButton to task column

--- src/components/dashboard/pages/projects/list/ProjectTask.tsx
+++ src/components/dashboard/pages/projects/list/ProjectTask.tsx
@@ -4,6 +4,7 @@ import AutoResizingInput from "@/components/styledElements/AutoResizingInput";
 import useProjectsDataContext from "@/utils/hooks/context/useProjectDataProvider";
 import { useParams } from "next/navigation";
 import DatePickerField from "@/components/styledElements/DatePickerField";
+import StatusButton from "@/components/styledElements/StatusButton";
 
 interface Props {
   taskName: string,
@@ -18,11 +19,11 @@ export const ProjectTask: React.FC<Props> = ({ taskName, taskId }) => {
   const task = project?.tasks?.find((task) => task.id === taskId);
 
   if (!project || !task) {
-    return <div className='mt-5 ml-8 text-2xl'>Can't retrieve data</div>;
+    return <div className='mt-5 ml-8 text-2xl'>No task found</div>;
   }
 
   return (
-    <div className="w-full">
+    <div className="w-full h-full">
       <div className={`grid grid-cols-10 grid-rows-1 border-b-2 border-undertone gap-2 h-10 transition-all duration-300`}>
         <div className={`flex col-span-4 border-r-2 border-undertone`}>
           <button className="mr-1 ml-5"><BadgeCheck size={22} color="green" /></button>
@@ -34,8 +35,8 @@ export const ProjectTask: React.FC<Props> = ({ taskName, taskId }) => {
           <DatePickerField datePickerStyles={`w-[50%] text-sm border-b-2 border-secondary-text bg-secondary text-primary-text`} selectedDate={task.dueDate} onDateChange={(newDate) => { if (newDate) { updateTaskDatabase(task, project, 'dueDate', newDate) } else { console.error('Invalid date selected: null') } }} />
         </div>
 
-        <div className={`flex items-center justify-between border-r-2 border-undertone col-span-2`}>
-          <button className="w-full pl-1 text-primary-text"><div className="h-3 w-3 border rounded-full border-white bg-white" /></button>
+        <div className={`w-full flex items-center border-r-2 border-undertone col-span-2`}>
+          <StatusButton project={project} task={task} model="task" status={task.status || 'No Status'} />
         </div>
 
         <div className="self-center col-span-2">
