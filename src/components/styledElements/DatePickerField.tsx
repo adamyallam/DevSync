26456f6794f4797c9cbabbe1464b4972cc6b408a
Adam Allam <adamyallam@gmail.com>
2025-01-11 00:03:58 -0600

    Added prop to change date format, format defaults to 'yyyy-mm-dd'

--- src/components/styledElements/DatePickerField.tsx
+++ src/components/styledElements/DatePickerField.tsx
@@ -6,15 +6,16 @@ interface Props {
   selectedDate: Date | null;
   onDateChange: (date: Date | null) => void;
   datePickerStyles?: string;
+  dateFormat?: string
 }
 
-const DatePickerField: React.FC<Props> = ({ selectedDate, onDateChange, datePickerStyles }) => {
+const DatePickerField: React.FC<Props> = ({ selectedDate, onDateChange, datePickerStyles, dateFormat = 'yyyy-MM-dd' }) => {
   return (
     <>
       <DatePicker
         selected={selectedDate}
         onChange={(date: Date | null) => onDateChange(date)}
-        dateFormat="yyyy-MM-dd" // Customize format
+        dateFormat={dateFormat}
         placeholderText="Select a date"
         className={`${datePickerStyles}`}
         wrapperClassName="datePicker"
