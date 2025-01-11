3cbe79c772c469070c4acce8d61d0043813817e9
Adam Allam <adamyallam@gmail.com>
2025-01-11 00:03:19 -0600

    adjusted styling and added functionality to complete/check button

--- src/components/dashboard/pages/projects/list/ProjectTask.tsx
+++ src/components/dashboard/pages/projects/list/ProjectTask.tsx
@@ -1,11 +1,12 @@
 'use client'
-import { Plus, BadgeCheck, CalendarClock, ChevronDown, UserRoundSearch } from "lucide-react"
+import { Check, CalendarClock, } from "lucide-react"
 import AutoResizingInput from "@/components/styledElements/AutoResizingInput";
 import useProjectsDataContext from "@/utils/hooks/context/useProjectDataProvider";
 import { useParams } from "next/navigation";
 import DatePickerField from "@/components/styledElements/DatePickerField";
 import StatusButton from "@/components/styledElements/StatusButton";
 import PriorityButton from "@/components/styledElements/PriorityButton";
+import { useState } from "react";
 
 interface Props {
   taskName: string,
@@ -25,15 +26,18 @@ export const ProjectTask: React.FC<Props> = ({ taskName, taskId }) => {
 
   return (
     <div className="w-full h-full">
-      <div className={`grid grid-cols-10 grid-rows-1 border-b-2 border-undertone gap-2 h-10 transition-all duration-300`}>
+      <div className={`grid grid-cols-10 grid-rows-1 border-b-2 border-undertone gap-2 h-10 hover:bg-selected group`}>
         <div className={`flex col-span-4 border-r-2 border-undertone`}>
-          <button className="mr-1 ml-5 hover:scale-110 transition-transform"><BadgeCheck size={22} color="green" /></button>
-          <AutoResizingInput initialWidth={440} maxGrowthWidth={440} placeholder="Name" initialText={taskName} textStyles="text-sm" onConfirmChange={(newName) => updateTaskDatabase(task, project, 'name', newName)} />
+          <button onClick={() => updateTaskDatabase(task, project, 'completed', !task.completed)} className="mr-1 ml-5 hover:scale-110 transition-transform">
+            <div className={`flex items-center justify-center w-[19px] h-[19px] border-2 rounded-full border-green-700 ${task.completed ? 'bg-green-600' : ''} transition-colors`}><Check className="ml-[1px] mt-[1px]" size={10} strokeWidth={3} color="white"/></div>
+          </button>
+
+          <AutoResizingInput initialWidth={150} maxGrowthWidth={444} placeholder="Name" initialText={taskName} textStyles="text-sm" onConfirmChange={(newName) => updateTaskDatabase(task, project, 'name', newName)} />
         </div>
 
         <div className={`flex items-center gap-2 border-r-2 border-undertone col-span-2 group`}>
           <button className={`pl-1 ${task.dueDate ? 'text-primary-text' : 'text-secondary-text'}`}><CalendarClock size={18} /></button>
-          <DatePickerField datePickerStyles={`w-[50%] text-sm border-b-2 border-secondary-text bg-secondary text-primary-text hover:cursor-pointer group-hover:scale-[1.05] transition-transform`} selectedDate={task.dueDate} onDateChange={(newDate) => { if (newDate) { updateTaskDatabase(task, project, 'dueDate', newDate) } else { console.error('Invalid date selected: null') } }} />
+          <DatePickerField datePickerStyles={`w-[50%] px-1 text-sm bg-secondary group-hover:bg-selected text-primary-text hover:cursor-pointer outline-none hover:outline hover:outline-2 hover:outline-primary focus:outline focus:outline-2 focus:outline-secondary-text rounded-sm`} dateFormat="MMMM d" selectedDate={task.dueDate} onDateChange={(newDate) => { if (newDate) { updateTaskDatabase(task, project, 'dueDate', newDate) } else { console.error('Invalid date selected: null') } }} />
         </div>
 
         <div className="w-full flex items-center border-r-2 border-undertone col-span-2">
