00b1affa430b366160408de1ff808caa11ee2690
Adam Allam <adamyallam@gmail.com>
2025-01-17 01:50:26 -0600

    added project view to change which projects are displayed

--- src/components/dashboard/pages/home/ProjectsCard.tsx
+++ src/components/dashboard/pages/home/ProjectsCard.tsx
@@ -5,16 +5,45 @@ import ProjectBoxLink from "@/components/styledElements/ProjectBoxLink"
 import { BouncingDots } from "@/components/styledElements/LoadingElements"
 import { StatusKey } from "@/utils/statusConfig"
 import useNavbarUIContext from "@/utils/hooks/context/useNavbarUIContext"
+import { useState, useRef } from "react"
+import useMenuClose from "@/utils/hooks/useMenuClose"
 
 export const ProjectsCard = () => {
   const { projects, loading } = useProjectsDataContext()
   const { isCreateProjectFormOpen, toggleCreateProjectForm } = useNavbarUIContext();
 
+  const projectsViewMenu = useRef<HTMLDivElement>(null)
+  const projectsViewButton = useRef<HTMLButtonElement>(null)
+
+  const [projectsView, setProjectsView] = useState('All Projects')
+  const [openProjectsView, setOpenProjectsView] = useState(false)
+
+  useMenuClose(projectsViewMenu, projectsViewButton, openProjectsView, setOpenProjectsView)
+
+  const filteredProjects = projects?.filter((project) => {
+    if (projectsView === 'Favorited') {
+      return project.favorited
+    }
+    if (projectsView === 'Unfavorited') {
+      return !project.favorited
+    }
+    return true
+  })
+
   return (
     <div className="w-550 h-96 border-2 border-undertone rounded-lg shadow-sm overflow-auto">
       <div className="flex items-center mt-6 ml-8">
-        <h1 className="text-2xl text-primary-text">Projects</h1>
-        <button className="flex items-center gap-0.5 text-xs ml-2 text-secondary-text hover:font-semibold hover:scale-105 transition-transform">Recents<ChevronDown size={14} strokeWidth={3} className="mt-1" /></button>
+        <h1 className="text-2xl text-primary-text font-semibold">Projects</h1>
+        <div className="relative">
+          <button ref={projectsViewButton} onClick={() => setOpenProjectsView((prev) => !prev)} className="flex items-center gap-0.5 text-xs ml-2 text-secondary-text hover:font-semibold hover:scale-105 transition-transform">{projectsView}<ChevronDown size={14} strokeWidth={3} /></button>
+          {openProjectsView && (
+            <div ref={projectsViewMenu} className="absolute flex flex-col items-center w-32 bg-primary border-2 border-undertone rounded-md shadow-md z-50">
+              <button onClick={() => { setProjectsView('All Projects'), setOpenProjectsView(false) }} className={`text-primary-text hover:bg-selected w-full  ${projectsView === 'All Projects' ? 'hidden' : ''}`}>All Projects</button>
+              <button onClick={() => { setProjectsView('Favorited'), setOpenProjectsView(false) }} className={`text-primary-text hover:bg-selected w-full ${projectsView === 'Favorited' ? 'hidden' : ''}`}>Favorited</button>
+              <button onClick={() => { setProjectsView('Unfavorited'), setOpenProjectsView(false) }} className={`text-primary-text hover:bg-selected w-full  ${projectsView === 'Unfavorited' ? 'hidden' : ''}`}>Unfavorited</button>
+            </div>
+          )}
+        </div>
       </div>
 
       <div className="grid grid-cols-2 gap-y-6 ml-8 w-[90%] mt-8 pb-5">
@@ -38,7 +67,7 @@ export const ProjectsCard = () => {
               <span className="ml-3 text-secondary-text text-sm font-semibold">Create project</span>
             </button>
 
-            {projects.map((project) => (
+            {filteredProjects?.map((project) => (
               <ProjectBoxLink key={String(project.id)} projectID={project.id} name={project.name} status={project.status ? project.status : "SetStatus" as StatusKey} />
             ))}
           </>
