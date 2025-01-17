b49bb107350ced4205efe00b32dba4e14644fe7e
Adam Allam <adamyallam@gmail.com>
2025-01-17 00:30:13 -0600

    Added functionality and tasks to taskCard. Updated and changed styling

--- src/components/dashboard/pages/home/TasksCard.tsx
+++ src/components/dashboard/pages/home/TasksCard.tsx
@@ -1,22 +1,38 @@
 'use client'
 import { useState } from "react"
 import { useSession } from 'next-auth/react';
+import useProjectsDataContext from "@/utils/hooks/context/useProjectDataProvider";
+import { BouncingDots } from "@/components/styledElements/LoadingElements";
+import { Check, ChevronDown } from "lucide-react";
+import AutoResizingInput from "@/components/styledElements/AutoResizingInput";
+import DatePickerField from "@/components/styledElements/DatePickerField";
 
-// Component Imports
 
 export const TasksCard = () => {
+  const { projects, loading, updateTaskDatabase } = useProjectsDataContext()
   const { data: session } = useSession()
+
+  const [taskView, setTaskView] = useState('Upcoming')
+  const [isProjectCollapsed, setIsProjectCollapsed] = useState<Record<string, boolean>>({})
 
   const userInitials = session ? session?.firstName.trim()[0] + session?.lastName.trim()[0] : 'A'
 
-  const [taskView, setTaskView] = useState('Upcoming')
+  const toggleProjectCollapse = (projectId: number) => {
+    setIsProjectCollapsed((prev) => ({
+      ...prev,
+      [projectId]: !prev[projectId],
+    }));
+  };
+
+  if (!projects) return null
 
   return (
-    <div className="w-550 h-96 border-2 border-undertone rounded-lg shadow-sm">
-
-      <div className="border-b-2 border-undertone">
+    <div className="w-550 h-96 border-2 border-undertone rounded-lg shadow-sm flex flex-col overflow-hidden">
+      <div className="border-b-2 border-undertone sticky top-0 bg-secondary z-10">
         <div className="flex items-center mt-5 ml-12 pb-3">
-          <button className='border-2 border-primary-text rounded-full w-10 h-10 p-1 text-lg text-primary-text mr-3 hover:scale-105 hover:border-secondary-text hover:text-secondary-text transition-transform'>{userInitials}</button>
+          <button className="border-2 border-primary-text rounded-full w-10 h-10 p-1 text-lg text-primary-text mr-3 hover:scale-105 hover:border-secondary-text hover:text-secondary-text transition-transform">
+            {userInitials}
+          </button>
 
           <div className="flex flex-col gap-2 ml-5 text-primary-text">
             <h1 className="text-lg font-semibold">My Tasks</h1>
@@ -25,14 +41,27 @@ export const TasksCard = () => {
               {[
                 { name: 'Upcoming' },
                 { name: 'Overdue' },
-                { name: 'Completed' }
+                { name: 'Completed' },
               ].map(({ name }) => (
                 <div key={name} className="flex-col ml-1">
                   <div className="flex">
-                    <span className={`invisible scale-105 font-semibold text-xs`}>{name}</span>
-                    <button onClick={() => setTaskView(name)} className={`absolute ${taskView === name ? 'scale-110 font-semibold' : 'hover:scale-110 hover:font-semibold transition-all'} text-xs`}>{name}</button>
+                    <span className="invisible absolute scale-105 font-semibold text-xs">
+                      {name}
+                    </span>
+                    <button
+                      onClick={() => setTaskView(name)}
+                      className={`${taskView === name
+                        ? 'scale-110 font-semibold'
+                        : 'hover:scale-110 hover:font-semibold transition-all'
+                        } text-xs`}
+                    >
+                      {name}
+                    </button>
                   </div>
-                  <div className={`${taskView === name ? 'scale-x-100' : 'scale-x-0'} bg-primary-text w-full h-[2px] translate-y-[13px] transition-transform duration-500`} />
+                  <div
+                    className={`${taskView === name ? 'scale-x-100' : 'scale-x-0'
+                      } bg-primary-text w-full h-[2px] translate-y-[13px] transition-transform duration-500`}
+                  />
                 </div>
               ))}
             </div>
@@ -40,8 +69,57 @@ export const TasksCard = () => {
         </div>
       </div>
 
-      <div className="flex flex-col text-primary-text font-semibold">
+      <div className="flex-grow overflow-y-auto p-5">
+        {loading ? (
+          <div className="flex justify-center mt-24">
+            <BouncingDots color="#DBDADA" size={15} />
+          </div>
+        ) : !projects?.length ? (
+          <div className="flex flex-col items-center p-5">
+            <span className="text-primary-text">You have no tasks!</span>
+            <span className="text-primary-text">When you create some, they will appear here!</span>
+          </div>
+        ) : (
+          <>
+            {projects.map((project) => (
+              <div className="flex flex-col pb-5">
+                <div className="flex gap-1 items-center pb-1">
+                  <button onClick={() => toggleProjectCollapse(project.id)} className={`${isProjectCollapsed[project.id] ? '-rotate-90' : 'rotate-0'} hover:scale-110 text-secondary-text hover:text-primary-text duration-500 ease-in-out transition-transform`}>
+                    <ChevronDown size={16} strokeWidth={3} />
+                  </button>
+                  <span className="text-primary-text font-semibold">
+                    {project.name}
+                  </span>
+                </div>
 
+                <div className={`${isProjectCollapsed[project.id] ? 'hidden' : ''} mt-2`}>
+                  {project.tasks.filter((task) => !task.completed).map((task, index) => (
+                    <div className={`flex flex-col border-t border-undertone w-full ${index === project.tasks.filter((task) => !task.completed).length - 1 ? '' : 'border-b'}`}>
+                      <div className="flex gap-2 p-2 items-center justify-between">
+                        <div className="flex">
+                          <button onClick={() => updateTaskDatabase(task, project, 'completed', !task.completed)} className="ml-3 hover:scale-110 transition-transform">
+                            <div className={`flex items-center justify-center w-[19px] h-[19px] border-2 rounded-full border-green-700 ${task.completed ? 'bg-green-600' : ''} transition-colors`}>
+                              <Check className="ml-[1px] mt-[1px]" size={10} strokeWidth={3} color="white" />
+                            </div>
+                          </button>
+                          <span className="text-primary-text text-sm">
+                            <AutoResizingInput textStyles="text-sm" initialWidth={150} maxGrowthWidth={275} initialText={task.name || ''} placeholder="Name..." onConfirmChange={(newName) => updateTaskDatabase(task, project, 'name', newName)} />
+                          </span>
+                        </div>
+
+                        <div>
+                          <DatePickerField datePickerStyles="w-[55%] px-1 text-sm bg-secondary text-primary-text hover:cursor-pointer outline-none hover:outline hover:outline-2 hover:outline-primary focus:outline focus:outline-2 focus:outline-secondary-text rounded-sm" selectedDate={task.dueDate} onDateChange={(newDate) => { if (newDate) { updateTaskDatabase(task, project, 'dueDate', newDate) } else { console.error('Invalid date selected: null') } }}/>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+
+                <div className="bg-secondary-text w-full h-[2px] ml-1 mt-[2px]" />
+              </div>
+            ))}
+          </>
+        )}
       </div>
     </div>
   )
