6927959a4d6afe6c0eaa2aaee489707191db6d52
Adam Allam <adamyallam@gmail.com>
2025-02-11 14:40:33 -0600

    Added logout functionality and converted buttons to Link tags

--- src/components/main/navbar/TopBar.tsx
+++ src/components/main/navbar/TopBar.tsx
@@ -1,18 +1,23 @@
 'use client'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
 import LogoBig from 'src/assets/imgs/LogoBig.png'
 import { useSession } from 'next-auth/react';
-
-
+import useMenuClose from '@/utils/hooks/useMenuClose';
+import { signOut } from 'next-auth/react';
 
 export default function TopBar() {
   const { data: session } = useSession()
 
-  const router = useRouter();
   const [hasScrolled, setHasScrolled] = useState(false);
+  const [logoutOpen, setLogoutOpen] = useState(false);
+
+  const logoutMenu = useRef<HTMLDivElement>(null)
+  const logoutMenuButton = useRef<HTMLButtonElement>(null)
+
+  useMenuClose(logoutMenu, logoutMenuButton, logoutOpen, setLogoutOpen)
+  
 
   const userInitials = session ? session?.firstName.trim()[0] + session?.lastName.trim()[0] : 'A'
 
@@ -42,13 +47,18 @@ export default function TopBar() {
       </div>
       <div className='flex items-center gap-4 mr-3'>
         {session ? (
-          <div>
-            <button className='border-2 border-primary-text rounded-full w-9 h-9 p-1 text-sm text-primary-text mr-2 hover:scale-105 hover:border-secondary-text hover:text-secondary-text transition-transform'>{userInitials}</button>
+          <div className='relative flex flex-col items-center gap-5'>
+            <button ref={logoutMenuButton} onClick={() => setLogoutOpen(prev => !prev)} className='border-2 border-primary-text rounded-full w-9 h-9 p-1 text-sm text-primary-text mr-3 hover:scale-105 hover:border-secondary-text hover:text-secondary-text transition-transform'>{userInitials}</button>
+            {logoutOpen && (
+              <div ref={logoutMenu} className='absolute right-[1px] top-12 bg-primary hover:bg-secondary rounded-md shadow-xl p-2'>
+                <button onClick={() => signOut()} className='flex text-sm text-primary-text transition-colors'>Logout</button>
+              </div>
+            )}
           </div>
         ) : (
           <div className='flex gap-4'>
-            <button className='border-2 border-primary-text text-primary-text hover:scale-105 hover:border-secondary-text hover:text-secondary-text p-1 h-8 transition-transform text-sm rounded-sm' onClick={() => router.push('user/registration/signin')}>Sign in</button>
-            <button className='border-2 border-primary-text text-primary-text hover:scale-105 hover:border-secondary-text hover:text-secondary-text p-1 h-8 transition-transform text-sm rounded-sm' onClick={() => router.push('user/registration/signup')}>Sign up</button>
+            <Link href={'/user/registration/signin'} className='border-2 border-primary-text text-primary-text hover:scale-105 hover:border-secondary-text hover:text-secondary-text p-1 h-8 transition-transform text-sm rounded-sm'>Sign in</Link>
+            <Link href={'/user/registration/signup'} className='border-2 border-primary-text text-primary-text hover:scale-105 hover:border-secondary-text hover:text-secondary-text p-1 h-8 transition-transform text-sm rounded-sm' >Sign up</Link>
           </div>
         )}
       </div>
