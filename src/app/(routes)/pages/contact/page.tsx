ccaece29235c5252f93c651e706c9f215c0d64a6
Adam Allam <adamyallam@gmail.com>
2025-02-26 00:56:24 -0600

    Updated social links. Changed instagram to X

--- src/app/(routes)/pages/contact/page.tsx
+++ src/app/(routes)/pages/contact/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import { Linkedin, Instagram, Github } from 'lucide-react'
+import { Linkedin, Github } from 'lucide-react'
+import { FaXTwitter } from 'react-icons/fa6'; 
 
 const contact = () => {
     return (
@@ -12,8 +13,8 @@ const contact = () => {
                 <p className="text-lg text-primary-text">hello@adamyallam.com</p>
                 <div className="flex gap-4 items-center justify-center mt-5">
                     <Link className="text-primary-text hover:scale-105 hover:text-blue-400 transition-all duration-300" href={'https://www.linkedin.com/in/adamyallam/'}><Linkedin size={30} /></Link>
-                    <Link className="text-primary-text hover:scale-105 hover:text-blue-400 transition-all duration-300" href={'https://www.instagram.com/'}><Instagram size={30} /></Link>
-                    <Link className="text-primary-text hover:scale-105 hover:text-blue-400 transition-all duration-300" href={'https://www.github.com/'}><Github size={30} /></Link>
+                    <Link className="text-primary-text hover:scale-105 hover:text-blue-400 transition-all duration-300" href={'https://x.com/adamallam__'}><FaXTwitter size={30} /></Link>
+                    <Link className="text-primary-text hover:scale-105 hover:text-blue-400 transition-all duration-300" href={'https://github.com/adamyallam'}><Github size={30} /></Link>
                 </div>
             </div>
         </div>
