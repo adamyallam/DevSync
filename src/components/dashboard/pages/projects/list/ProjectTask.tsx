dc713cc10ce287f324b220ec58b44015ee59792b
Adam Allam <adamyallam@gmail.com>
2025-03-10 14:50:15 -0500

    Name increases in size when more space becomes available upon closing the sidebar

--- src/components/dashboard/pages/projects/list/ProjectTask.tsx
+++ src/components/dashboard/pages/projects/list/ProjectTask.tsx
@@ -91,7 +91,7 @@ export const ProjectTask: React.FC<Props> = ({ taskName, taskId, createTask, foc
           <div className={`flex items-center justify-center w-[19px] h-[19px] border-2 rounded-full border-green-700 ${task.completed ? 'bg-green-600' : ''} transition-colors`}><Check className="ml-[1px] mt-[1px]" size={10} strokeWidth={3} color="white" /></div>
         </button>
 
-        <AutoResizingInput focusInput={focusTask} initialWidth={150} maxGrowthWidth={444} placeholder="Name" initialText={taskName} textStyles="text-sm" onConfirmChange={(newName) => updateTaskDatabase(task, project, 'name', newName)} />
+        <AutoResizingInput focusInput={focusTask} initialWidth={150} maxGrowthWidth={isSidebarOpen ? 444 : 535} placeholder="Name" initialText={taskName} textStyles="text-sm" onConfirmChange={(newName) => updateTaskDatabase(task, project, 'name', newName)} />
       </div>
 
       <div className={`flex items-center gap-2 border-r-2 border-undertone col-span-2 group`}>
